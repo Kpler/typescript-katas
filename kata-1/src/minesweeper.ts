d626e30c263be31de61b23e4b5a9ebb7bfe985a2
Grégoire Chauvet <gchauvet.home@gmail.com>
2022-08-09 12:35:55 +0200

    First pass

--- kata-1/src/minesweeper.ts
+++ kata-1/src/minesweeper.ts
@@ -11,17 +11,25 @@ export const splitInputInFields = (input: string): MineSweeper[] => {
   const instruction = parseDimensions(byLine[globalIndex]);
   
   let linesToRead = instruction[0];
+  const output = [];
 
   while (linesToRead > 0) {
+    debugger;
+    globalIndex += 1;
+    const mineSweeperLines = byLine.slice(globalIndex, globalIndex + linesToRead);
+    output.push(new MineSweeper());
+
+    globalIndex += linesToRead;
+    linesToRead = parseDimensions(byLine[globalIndex])[0];
+
     // slice input as we know dimensions
     // store in an array
     // update global index with how many lines we've just read
 
 
-    globalIndex++;
-    const currentLine = byLine[globalIndex];
-    linesToRead--;
+    // const currentLine = byLine[globalIndex];
+    // linesToRead--;
   }
 
-  return []
+  return output
 };
