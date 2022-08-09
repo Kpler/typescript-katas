8c2e498cda07d5769937c1f39a45a13cc4da7df0
Cassie Brooks <cassbrooks@outlook.com>
2022-08-09 12:27:21 +0200

    add comments for while loop

--- kata-1/src/minesweeper.ts
+++ kata-1/src/minesweeper.ts
@@ -1,13 +1,27 @@
 class MineSweeper {
 }
 
+const parseDimensions = (line: string): [number, number] => {
+  return line.split(' ').map(x => Number(x)) as [number, number]
+}
+
 export const splitInputInFields = (input: string): MineSweeper[] => {
   const byLine = input.split("\n");
-  const instruction = byLine[0]
+  let globalIndex = 0;
+  const instruction = parseDimensions(byLine[globalIndex]);
+  
+  let linesToRead = instruction[0];
+
+  while (linesToRead > 0) {
+    // slice input as we know dimensions
+    // store in an array
+    // update global index with how many lines we've just read
+
+
+    globalIndex++;
+    const currentLine = byLine[globalIndex];
+    linesToRead--;
+  }
 
   return []
 };
-
-const parseField = (line: string): [number, number] => {
-  return line.split(' ').map(x => Number(x)) as [number, number]
-}
