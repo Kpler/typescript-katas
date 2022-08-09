bb21c34f31b84e84c7b9950e68f4380eac397938
Grégoire Chauvet <gchauvet.home@gmail.com>
2022-08-09 13:58:13 +0200

    Make display

--- kata-1/src/minesweeper.spec.ts
+++ kata-1/src/minesweeper.spec.ts
@@ -1,4 +1,4 @@
-import { parseMinesweepers } from "./minesweeper";
+import { Minesweeper, parseMinesweepers } from "./minesweeper";
 
 const input = `4 4
 *...
@@ -12,18 +12,30 @@ const input = `4 4
 0 0`;
 
 describe("Minesweeper", () => {
-  describe("parseMinesweepers()", () => {
+  describe("parseMinesweepers", () => {
     it("should return an array of two fields", () => {
       let actual = parseMinesweepers(input);
       expect(actual.length).toStrictEqual(2);
 
-      let minesweeper = actual[0];
-      expect(minesweeper.dimensions.rows).toBe(4);
-      expect(minesweeper.dimensions.cols).toBe(4);
-
-      minesweeper = actual[1];
-      expect(minesweeper.dimensions.rows).toBe(3);
-      expect(minesweeper.dimensions.cols).toBe(5);
+      const [firstMinesweeper, secondMinesweeper] = actual;
+      expect(firstMinesweeper.dimensions).toStrictEqual({ rows: 4, columns: 4 });
+      expect(secondMinesweeper.dimensions).toStrictEqual({ rows: 3, columns: 5 });
     });
+  });
+
+  describe("Minesweeper", () => {
+    it("should display mine fields", () => {
+      const minesweeper = new Minesweeper({ rows: 4, columns: 4 }, [{ row: 0, column: 0 }]);
+      expect(minesweeper.display).toBe(`*1..
+11..
+....
+....`)
+
+      const minesweeper2 = new Minesweeper({ rows: 4, columns: 4 }, [{ row: 0, column: 0 }, { row: 0, column: 1 }, { row: 2, column: 0 }]);
+      expect(minesweeper2.display).toBe(`**1.
+331.
+*1..
+11..`)
+    })
   });
 });
--- kata-1/src/minesweeper.ts
+++ kata-1/src/minesweeper.ts
@@ -1,36 +1,90 @@
 interface FieldDimension {
   rows: number;
-  cols: number;
+  columns: number;
 }
 
-class Minesweeper {
-  constructor(public readonly dimensions: Readonly<FieldDimension>) {}
+interface Position {
+  row: number;
+  column: number;
+}
+
+export class Minesweeper {
+  constructor(readonly dimensions: Readonly<FieldDimension>, readonly mines: Position[]) {}
+
+  static fromRawLines(dimensions: Readonly<FieldDimension>, rawLines: string[]): Minesweeper {
+    // @TODO: not implemented
+    return new Minesweeper(dimensions, []);
+  }
+
+  #hasMine(row: number, column: number): boolean {
+    // @TODO improve complexity
+    return this.mines.some(mine => mine.row === row && mine.column === column);
+  }
+
+  #getBoundedValue(row: number, column: number): number {
+    if (row < 0 || column < 0 || row > this.dimensions.rows || column > this.dimensions.columns) {
+      return 0;
+    }
+
+    return this.#hasMine(row, column) ? 1 : 0;
+  }
+
+  #countNeighborMines(row: number, column: number): number {
+    const neighborsCoordinates: Position[] = [
+      { row: -1, column: -1},
+      { row: 0, column: -1},
+      { row: 1, column: -1},
+      { row: -1, column: 0},
+      { row: 1, column: 0},
+      { row: -1, column: 1},
+      { row: 0, column: 1},
+      { row: 1, column: 1},
+    ]
+
+    return neighborsCoordinates.reduce((acc, { row: x, column: y }) => {
+      return acc + this.#getBoundedValue(row + x, column + y);
+    }, 0)
+  }
+
+  get display(): string {
+    const rows = [];
+    for (let i = 0; i < this.dimensions.rows; i++) {
+      let row = "";
+      for (let j = 0; j < this.dimensions.columns; j++) {
+        const neightborMines = this.#countNeighborMines(i, j);
+        row += this.#hasMine(i, j) ? "*" : neightborMines === 0 ? "." : neightborMines.toString();
+      }
+      rows.push(row);
+    }
+
+    return rows.join('\n');
+  }
 }
 
 const parseDimensions = (line: string): FieldDimension => {
-  const [rows, cols] = line.split(" ").map((x) => Number(x));
-  return { rows, cols };
+  const [rows, columns] = line.split(" ", 2).map((x) => Number(x));
+  return { rows, columns };
 };
 
 export const parseMinesweepers = (input: string): Minesweeper[] => {
-  const byLine = input.split("\n");
+  const lines = input.split("\n");
   let globalIndex = 0;
-  const instruction = parseDimensions(byLine[globalIndex]);
 
-  let linesToRead = instruction.rows;
   const output: Minesweeper[] = [];
 
-  while (linesToRead > 0) {
-    // debugger;
+  let dimensions = parseDimensions(lines[globalIndex]);
+  while (dimensions.rows !== 0) {
+    const linesToRead = dimensions.rows;
+
     globalIndex += 1;
-    const minesweeperLines = byLine.slice(
+    const minesweeperLines = lines.slice(
       globalIndex,
       globalIndex + linesToRead
     );
-    output.push(new Minesweeper());
+    output.push(Minesweeper.fromRawLines(dimensions, minesweeperLines));
 
     globalIndex += linesToRead;
-    linesToRead = parseDimensions(byLine[globalIndex]).rows;
+    dimensions = parseDimensions(lines[globalIndex]);
   }
 
   return output;
