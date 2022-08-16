0b0c15626cc79f03e618e76f05e37f2e309a85a9
akulenovic97 <akulenovic97@gmail.com>
2022-08-16 11:48:51 -0400

    working spare test

--- typescript-backend-katas/src/bowling_game/game.ts
+++ typescript-backend-katas/src/bowling_game/game.ts
@@ -3,20 +3,21 @@ export class Game {
     currentScore: number = 0
 
     score(): number {
-        for (let i = 0; i < this.rolls.length; i++){
+        for (let i = 0; i < this.rolls.length; i++) {
             this.currentScore += this.rolls[i]
-            const isSpare = i > 0 && i % 2 != 0
-                && this.rolls[i-1] + this.rolls[i-2] == 10
-            console.log.("this is the score", this.currentScore)
-            if (isSpare){
-            console.log.("we have a spare")
+            if (this.isSpare(i)) {
                 this.currentScore += this.rolls[i]
-                }
+            }
         }
         return this.currentScore
     }
 
     roll(pins: number) {
         this.rolls.push(pins)
+    }
+
+    private isSpare(i: number): boolean {
+        return i > 0 && i % 2 == 0
+                && this.rolls[i-1] + this.rolls[i-2] == 10
     }
 }
