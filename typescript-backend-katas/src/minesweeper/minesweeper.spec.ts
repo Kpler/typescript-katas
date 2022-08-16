7e3e32b4892ee19c55087cc5f2bd793d212eee29
Nicolas Guignard <nicolas065@gmail.com>
2022-08-16 11:37:28 +0200

    feat(minesweeper): most basic test and create function generate game

--- typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
+++ typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
@@ -1,6 +1,28 @@
+import { generateGame } from "./minesweeper"
 
-describe("Example test", () => {
-  it("should sum numbers correctly", () => {
-    expect(1 + 1).toBe(2);
+describe("Minesweeper when started ", () => {
+  it("should generate an empty grid of 3*3 ", () => {
+    // Given
+    const expectedResult = `...
+    ...
+    ...`
+
+    // When
+    const game = generateGame(3, 3, [])
+
+    // Then
+    expect(game).toBe(expectedResult);
+  });
+  it("should generate a grid of 3*3 with some mines ", () => {
+    // Given
+    const expectedResult = `**.
+    ...
+    ...`
+
+    // When
+    const game = generateGame(3, 3, [[0, 0], [0, 1]])
+
+    // Then
+    expect(game).toBe(expectedResult);
   });
 });
---
+++ typescript-backend-katas/src/minesweeper/minesweeper.ts
@@ -0,0 +1,7 @@
+export function generateGame(
+    rows: number, columns: number, mines: [number, number][]
+): string {
+    return `...
+    ...
+    ...`
+}
