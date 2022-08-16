5845dfb58a34f5709bf7b42e356e487def808e69
Henry Nomichith <hnomichith@kpler.com>
2022-08-16 18:47:33 +0200

    feat(bowlingGame): prepare for implementing strikes

--- typescript-backend-katas/src/bowling_game/game.spec.ts
+++ typescript-backend-katas/src/bowling_game/game.spec.ts
@@ -1,6 +1,6 @@
 import { Game } from "./game"
 
-describe("When playing Bowling ", () => {
+describe("When playing Bowling", () => {
     describe("if player throws always in the gutter ", () => {
         it("should return 0", () => {
             // Given
@@ -10,13 +10,13 @@ describe("When playing Bowling ", () => {
             }
 
             // When
-            const actualResult = game.score()
+            const score = game.score()
 
             // Then
-            expect(actualResult).toEqual(0)
+            expect(score).toEqual(0)
         });
     });
-    describe("if player takes down only one pin per roll ", () => {
+    describe("if player takes down only one pin per roll", () => {
         it("should add up correctly the basic number of pins", () => {
             // Given
             const game = new Game()
@@ -25,27 +25,80 @@ describe("When playing Bowling ", () => {
             }
 
             // When
-            const actualResult = game.score()
+            const score = game.score()
 
             // Then
-            expect(actualResult).toEqual(20)
+            expect(score).toEqual(20)
         });
     });
-    describe("if player has a spare ", () => {
+    describe("if player has a spare", () => {
         it("should add up the next number of pins twice", () => {
             // Given
             const game = new Game()
-            game.roll(3)
-            game.roll(7) //  10 + next roll (1) --> 11
-            for (let i= 0; i < 18; i++) {
-                game.roll(1)
-            }
+            game.roll(3);
+            game.roll(7); // 10 + next roll (1) --> 11
+            game.roll(1);
 
             // When
-            const actualResult = game.score()
+            const score = game.score();
 
             // Then
-            expect(actualResult).toEqual(29)
+            expect(score).toEqual(12);
         });
     });
+    describe("if player has two spares", () => {
+        it("should add up the next number of pins twice", () => {
+            // Given
+            const game = new Game()
+            game.roll(3);
+            game.roll(7); // 10 + 9 --> 19
+            game.roll(9);
+            game.roll(1); // 10 + next roll (1) --> 15
+            game.roll(5);
+
+            // When
+            const score = game.score();
+
+            // Then
+            expect(score).toEqual(39);
+        });
+    });
+
+    describe("roll", () => {
+        it("shall build the frames correctly", () => {
+            // Given
+            const game = new Game()
+
+            // When
+            game.roll(3);
+            game.roll(7);
+            game.roll(9);
+            game.roll(1);
+            game.roll(5);
+            const frames = game.frames;
+
+            // Then
+            expect(frames).toEqual([
+                // TODO
+            ]);
+        });
+    });
+
+    // TODO uncomment this method after having done the previous one
+    /*describe("if player has a strike", () => {
+        it("should add up the next two number of pins twice", () => {
+            // Given
+            const game = new Game()
+            game.roll(10); // 10 + 5 + 3 = 18
+            game.roll(5); // 
+            game.roll(3); 
+            game.roll(7);
+
+            // When
+            const score = game.score();
+
+            // Then
+            expect(score).toEqual(33);
+        });
+    });*/
 });
--- typescript-backend-katas/src/bowling_game/game.ts
+++ typescript-backend-katas/src/bowling_game/game.ts
@@ -1,15 +1,25 @@
 export class Game {
-    rolls: number[] = []
-    currentScore: number = 0
+  frames: number[][] = [[]];
 
-    score(): number {
-        for (let i = 0; i < this.rolls.length; i++){
-            this.currentScore += this.rolls[i]
+  score(): number {
+    let currentScore = 0;
+
+    for (let frameIndex = 0; frameIndex < this.frames.length; frameIndex++) {
+        const currentFrame = this.frames[frameIndex];
+      for (let rollIndex = 0; rollIndex < currentFrame.length; rollIndex++) {
+        // If it's a odd roll and the last rolls score is ten, then it's a spare
+        if (rollIndex%2 === 1 && currentFrame[rollIndex] + currentFrame[rollIndex-1] === 10){
+            currentScore += currentFrame[rollIndex+1];
         }
-        return this.currentScore
+        currentScore += currentFrame[rollIndex];
+      }
     }
+    return currentScore;
+  }
 
-    roll(pins: number) {
-        this.rolls.push(pins)
-    }
+  // TODO: modify the roll method to update not only the first frame
+  roll(pins: number) {
+    const lastFrame = this.frames[this.frames.length-1]
+    lastFrame.push(pins);
+  }
 }
