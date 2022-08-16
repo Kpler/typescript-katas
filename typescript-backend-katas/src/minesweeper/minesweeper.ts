f4757500cffb06a32e0f8a1f8f5635409bf663bd
Léo <ldureuil@kpler.com>
2022-08-16 17:47:04 +0800

    leo

--- typescript-backend-katas/src/minesweeper/minesweeper.ts
+++ typescript-backend-katas/src/minesweeper/minesweeper.ts
@@ -1,7 +1,12 @@
 export function generateGame(
     rows: number, columns: number, mines: [number, number][]
 ): string {
-    return `...
-    ...
-    ...`
+    let grid: string[][] = []
+
+    for (let i = 0; i < rows ; i++) {
+        for (let j = 0; j < columns; j++) {
+
+        }
+    }
+    return grid
 }
