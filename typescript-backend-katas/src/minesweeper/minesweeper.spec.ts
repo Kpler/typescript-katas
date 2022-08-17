2f72d4e37c701ef2924faabdbdb138e502fa401a
Nicolas Guignard <nicolas065@gmail.com>
2022-08-17 11:20:14 +0200

    test(minesweeper): create test for empty grid

--- typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
+++ typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
@@ -1,6 +1,27 @@
+class Minesweeper {
+  #rows: number
+  #columns: number
+  constructor(rows: number, columns: number, mines: [number, number][]) {
+    this.#rows = rows
+    this.#columns = columns
+  }
 
-describe("Example test", () => {
-  it("should sum numbers correctly", () => {
-    expect(1 + 1).toBe(2);
+  toString(): string {
+    return ``
+  }
+
+}
+
+describe("MineSweeper ", () => {
+  it("should generate a map with the right dimensions ", () => {
+    const expectedResult = `...
+    ...
+    ...
+    `
+
+    const game = new Minesweeper(3, 3, [])
+    const actual = game.toString()
+
+    expect(actual).toBe(expectedResult);
   });
 });
