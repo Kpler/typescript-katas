d0b892b058063218699639757e6e7277edc1405c
MOTRIEUX Thomas <tmotrieux@kpler.com>
2022-08-17 12:03:39 +0200

    🚧  WIP

--- typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
+++ typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
@@ -1,4 +1,4 @@
-import {MinesweeperGame} from "./minesweeper";
+import { MinesweeperGame } from "./minesweeper";
 
 describe("MinesweeperGame", () => {
     it("should print empty game", () => {
@@ -7,7 +7,12 @@ describe("MinesweeperGame", () => {
     });
 
     it("should print one mine", () => {
-        const minesweeperGame = new MinesweeperGame(2, 2, [[0, 0]])
-        expect(minesweeperGame.print()).toBe("*.\n..\n")
+        const minesweeperGame = new MinesweeperGame(2, 2, [[0, 1]])
+        expect(minesweeperGame.print()).toBe(".*\n..\n")
+    });
+
+    it("should print two mines", () => {
+        const minesweeperGame = new MinesweeperGame(2, 2, [[0, 1], [1, 1]])
+        expect(minesweeperGame.print()).toBe(".*\n.*\n")
     });
 });
--- typescript-backend-katas/src/minesweeper/minesweeper.ts
+++ typescript-backend-katas/src/minesweeper/minesweeper.ts
@@ -1,28 +1,26 @@
 export class MinesweeperGame {
   private rows: number
   private cols: number
+
   private minePositions: [number, number][]
+  private minePositionsMap: { [key: string]: boolean }
 
   constructor(rows: number, cols: number, minePositions: [number, number][]) {
     this.rows = rows
     this.cols = cols
-    this.minePositions = []
+    this.minePositions = minePositions
+    this.minePositionsMap = this.minePositions.reduce((acc, [r, c]) => ({
+      ...acc,
+      [`${r},${c}`]: true,
+    }), {});
   }
 
   print(): string {
-    let output : string = ""
-
-    const minePositionMap = this.minePositions.reduce((acc, [x, y]) => ({
-      ...acc,
-      [`${x},${y}`]: true,
-    }), {});
+    let output: string = ""
 
     for (let r = 0; r < this.rows; r++) {
       for (let c = 0; c < this.cols; c++) {
-
-        // if c and r match the tuple, use *
-
-        output += '.'
+        output += this.minePositionsMap[`${r},${c}`] ? '*' : '.'
       }
       output += "\n"
     }
