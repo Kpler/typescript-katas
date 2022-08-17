97eecff25f11d3d647c907f6d25198ed9b0057f9
Henry Nomichith <hnomichith@kpler.com>
2022-08-17 11:20:26 +0200

    henry's best commit ever

--- typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
+++ typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
@@ -1,6 +1,14 @@
+import { generateField } from "./minesweeper";
 
-describe("Example test", () => {
-  it("should sum numbers correctly", () => {
+describe("Our main method", () => {
+  it("should not break", () => {
+    // GIVEN
+    const rowsNumber = 1;
+    const colsNumber = 1;
+    const minesPositions: number[][] = [];
+    // WHEN
+    const result = generateField(rowsNumber, colsNumber, minesPositions);
+    // THEN
     expect(1 + 1).toBe(2);
   });
 });
---
+++ typescript-backend-katas/src/minesweeper/minesweeper.ts
@@ -0,0 +1,9 @@
+export const generateField = (rowsNumber: number, colsNumber: number, minesPositions: number[][]) => {
+  let result = "";
+  for (let rowIdx=0 ; rowIdx < rowsNumber ; rowIdx++) {
+    for (let colIdx=0 ; colIdx < colsNumber ; colIdx++) {
+      result += ""
+    }
+  }
+  console.log(result);
+}
