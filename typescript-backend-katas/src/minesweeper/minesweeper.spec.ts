a20a5ed09eeef7f3d6ff55b7b8eca123a910bfff
Damien GOUYETTE <dgouyette@kpler.com>
2022-08-17 12:10:17 +0200

    latest version

--- typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
+++ typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
@@ -1,45 +1,58 @@
 function generateGrid(rows: number,
-  columns: number,
-  mines: number[][]
+                      columns: number,
+                      mines: number[][]
 ): string[][] {
 
-  if (rows <= 0 || columns <= 0) {
-
-    throw new Error("This is not good")
-  }
-  let result: string[][] = []
-  for (let r = 0; r < rows; r++) {
-    let temp_row: string[] = []
-    for (let c = 0; c < columns; c++) {
-      temp_row.push(".")
+    if (rows <= 0 || columns <= 0) {
+        throw new Error("This is not good")
     }
-    result.push(temp_row)
-  }
-  return result;
+    let result: string[][] = []
+    for (let r = 0; r < rows; r++) {
+        let temp_row: string[] = []
+        for (let c = 0; c < columns; c++) {
+            if (columns == c && rows == r) {
+                temp_row.push("*")
+            } else {
+                temp_row.push(".")
+            }
+        }
+        result.push(temp_row)
+    }
+    return result;
 }
 
 
 describe("Example test", () => {
 
-  it("should generate an empty grid", () => {
-    expect(generateGrid(1, 1, [])).toEqual([['.']]);
-  });
+    it("should generate an empty grid", () => {
+        expect(generateGrid(1, 1, [])).toEqual([['.']]);
+    });
 
-  it("should generate an 3x3 grid", () => {
-    expect(generateGrid(3, 3, [])).toEqual([['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]);
-  });
+    it("should generate an 3x3 grid", () => {
+        expect(generateGrid(3, 3, [])).toEqual([['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']]);
+    });
 
-  it("row should be positive", () => {
+    it("row should be positive", () => {
+        expect(() => {
+            generateGrid(0, 3, []);
+        }).toThrow("This is not good");
+        expect(() => {
+            generateGrid(-1, 3, []);
+        }).toThrow("This is not good");
+    });
 
-    expect(() => { generateGrid(3, -1, []); }).toThrow("This is not good");
-    // expect(generateGrid(3, 3, [])).toThrow("This is not good");
-    //   expect(generateGrid(-1, 3, [])).toThrow(Error("This is not good"));
-  });
+    it("column should be positive", () => {
+        expect(() => {
+            generateGrid(3, 0, [])
+        }).toThrow(Error("This is not good"));
+        expect(() => {
+            generateGrid(3, -1, [])
+        }).toThrow(Error("This is not good"));
+    });
 
-  // it("column should be positive", () => {
-  //   expect(generateGrid(3, 0, [])).toThrow(Error("This is not good"));
-  //   expect(generateGrid(3, -1, [])).toThrow(Error("This is not good"));
-  // });
+    it("should generate 3x3 with 1 mine", () => {
+        expect(generateGrid(3, 3, [[2, 2]])).toEqual([['.', '.', '.'], ['.', '.', '.'], ['.', '.', '*']]);
+    });
 
 
 });
