b955960b776750839d35d3ae95a4858ca3bea9f2
Damien GOUYETTE <dgouyette@kpler.com>
2022-08-17 11:40:29 +0200

    feat(kata): empty grid

--- typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
+++ typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
@@ -1,22 +1,28 @@
-
-describe("Example test", () => {
-  it("should sum numbers correctly", () => {
-    expect(1 + 1).toBe(2);
-  });
-});
-
-
-function generateGrid(row: number,
-                      line: number,
+function generateGrid(rows: number,
+                      columns: number,
                       mines: number[][]
-) : number[][]{
-  const result: number[1][1];
-  return result;
+): string[][] {
+
+  let  result: string[][] = [[]]
+  for (let r = 0; r < rows; r++) {
+    for (let c = 0; c < columns; c++) {
+      result.push(["."])
+    }
+  }
+    return [['.']];
 }
 
 
 describe("Example test", () => {
-  it("should sum numbers correctly", () => {
-    generateGrid(1, 1, [][]).toBe(2);
+
+  it("should generate an empty grid", () => {
+    expect(generateGrid(1, 1, [])).toEqual([['.']]);
   });
+
+  it("should generate an 3x3 grid", () => {
+    expect(generateGrid(3, 3, [])).toEqual([['.','.','.'],['.','.','.'],['.','.','.']]);
+  });
+
+
+
 });
