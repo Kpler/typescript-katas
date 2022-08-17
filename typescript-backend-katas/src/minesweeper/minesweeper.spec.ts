4c169e1a9194bde132da2d7dbcb7010f0cbe7525
Cassie Brooks <cassbrooks@outlook.com>
2022-08-17 11:49:46 +0200

    feat(minesweeper): add minePositions param to class

--- typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
+++ typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
@@ -1,14 +1,13 @@
 import {MinesweeperGame} from "./minesweeper";
 
-
 describe("MinesweeperGame", () => {
     it("should print empty game", () => {
-        const minesweeperGame = new MinesweeperGame(2, 2)
+        const minesweeperGame = new MinesweeperGame(2, 2, [])
         expect(minesweeperGame.print()).toBe("..\n..\n")
     });
 
-    it("should print one bomb", () => {
-        const minesweeperGame = new MinesweeperGame(2, 2)
-        expect(minesweeperGame.print()).toBe("..\n..\n")
+    it("should print one mine", () => {
+        const minesweeperGame = new MinesweeperGame(2, 2, [[0, 0]])
+        expect(minesweeperGame.print()).toBe("*.\n..\n")
     });
 });
--- typescript-backend-katas/src/minesweeper/minesweeper.ts
+++ typescript-backend-katas/src/minesweeper/minesweeper.ts
@@ -1,18 +1,27 @@
 export class MinesweeperGame {
   private rows: number
   private cols: number
-  private bombs: [number,number][]
+  private minePositions: [number, number][]
 
-  constructor(rows: number, cols: number) {
+  constructor(rows: number, cols: number, minePositions: [number, number][]) {
     this.rows = rows
     this.cols = cols
-    this.bombs = []
+    this.minePositions = []
   }
 
   print(): string {
     let output : string = ""
+
+    const minePositionMap = this.minePositions.reduce((acc, [x, y]) => ({
+      ...acc,
+      [`${x},${y}`]: true,
+    }), {});
+
     for (let r = 0; r < this.rows; r++) {
       for (let c = 0; c < this.cols; c++) {
+
+        // if c and r match the tuple, use *
+
         output += '.'
       }
       output += "\n"
