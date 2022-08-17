11df57de304865c2498aeb9a5d67ebf847d19566
danchemla <dchemla@kpler.com>
2022-08-17 11:20:06 +0200

    first(step): enjoy

--- typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
+++ typescript-backend-katas/src/minesweeper/minesweeper.spec.ts
@@ -4,3 +4,19 @@ describe("Example test", () => {
     expect(1 + 1).toBe(2);
   });
 });
+
+
+function generateGrid(row: number,
+                      line: number,
+                      mines: number[][]
+) : number[][]{
+  const result: number[1][1];
+  return result;
+}
+
+
+describe("Example test", () => {
+  it("should sum numbers correctly", () => {
+    generateGrid(1, 1, [][]).toBe(2);
+  });
+});
