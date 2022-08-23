4e0181222c9f87a13da3bd53c09b9792b88bee21
Cassie Brooks <cassbrooks@outlook.com>
2022-08-23 17:30:42 +0200

    second striker run

--- typescript-backend-katas/src/bowling_game/game.ts
+++ typescript-backend-katas/src/bowling_game/game.ts
@@ -5,21 +5,31 @@ export class Game {
     let currentScore = 0;
 
     for (let frameIndex = 0; frameIndex < this.frames.length; frameIndex++) {
-        const currentFrame = this.frames[frameIndex];
+      const currentFrame = this.frames[frameIndex];
       for (let rollIndex = 0; rollIndex < currentFrame.length; rollIndex++) {
+
         // If it's a odd roll and the last rolls score is ten, then it's a spare
-        if (rollIndex%2 === 1 && currentFrame[rollIndex] + currentFrame[rollIndex-1] === 10){
-            currentScore += currentFrame[rollIndex+1];
+        const sum = currentFrame.reduce((a, b) => a + b, 0);
+        if (sum === 10) {
+          // spare
         }
+        // if (rollIndex % 2 === 1 && currentFrame[rollIndex] + currentFrame[rollIndex-1] === 10) {
+        //   currentScore += currentFrame[rollIndex+1];
+        // }
+
         currentScore += currentFrame[rollIndex];
       }
     }
     return currentScore;
   }
 
-  // TODO: modify the roll method to update not only the first frame
   roll(pins: number) {
-    const lastFrame = this.frames[this.frames.length-1]
+    const lastFrame = this.frames[this.frames.length-1];
+
     lastFrame.push(pins);
+
+    if (lastFrame.length === 2) {
+      this.frames.push([]);
+    }
   }
 }
