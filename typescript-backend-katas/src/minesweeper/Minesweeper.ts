dc5e32edb2b6b6fa3f0e3eb683959167f0bd1ddc
Grégoire Chauvet <gchauvet.home@gmail.com>
2022-08-31 11:56:44 +0200

    Refactor

--- typescript-backend-katas/src/minesweeper/Minesweeper.ts
+++ typescript-backend-katas/src/minesweeper/Minesweeper.ts
@@ -1,11 +1,16 @@
+enum Cell {
+  MINE = "*",
+  UNKOWN = ".",
+}
+
+type Field = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "*";
+// type Field = string;
+
 export class Minesweeper {
-  #grid: string[][];
-  #mines: [number, number][];
+  #grid: Field[][];
 
   constructor(rows: number, columns: number, mines: [number, number][]) {
-    this.#mines = mines;
-    this.#grid = this.#generateGrid(rows, columns);
-    this.#addMines();
+    this.#grid = this.#generateField(rows, columns, mines);
   }
 
   toString(): string {
@@ -13,46 +18,58 @@ export class Minesweeper {
   }
 
   toStringWithCounters(): string {
-    return this.#countMines(this.#grid).map(str => str.join("")).join("\n");
+    return this.#countMines(this.#grid)
+      .map((str) => str.join(""))
+      .join("\n");
   }
 
-  #addMines() {
-    this.#mines.forEach((mine) => {
-      const [mineX, mineY] = mine;
-      this.#grid[mineY][mineX] = "*";
-    });
+  #generateField(
+    rows: number,
+    columns: number,
+    mines: [number, number][]
+  ): Field[][] {
+    const gridWithMines = this.#generateGrid(rows, columns, mines);
+    return this.#countMines(gridWithMines, mines);
   }
 
-  #generateGrid(rows: number, columns: number): string[][] {
-    const grid = [];
+  #generateGrid(
+    rows: number,
+    columns: number,
+    mines: [number, number][]
+  ): Cell[][] {
+    const grid: Cell[][] = [];
 
     for (let i = 0; i < rows; i++) {
       const rowArr = [];
       for (let j = 0; j < columns; j++) {
-        rowArr.push(".");
+        rowArr.push(Cell.UNKOWN);
       }
       grid.push(rowArr);
     }
 
+    mines.forEach((mine) => {
+      const [mineX, mineY] = mine;
+      grid[mineY][mineX] = Cell.MINE;
+    });
+
     return grid;
   }
 
-  #countMines(grid: string[][]): (Cell | number)[][] {
-    return grid.map((row, rowIndex) => row.map((cell, cellIndex) => {
+  #countMines(grid: Cell[][], mines: [number, number][]): Field[][] {
+    return grid.map((row, rowIndex) =>
+      row.map((cell, cellIndex) => {
+        if (cell === Cell.MINE) {
+          return "*";
+        }
 
-      if (cell === "*") {
-        return Cell.MINE;
-      } else {
-        const filteredThing = this.#mines.filter(([y, x]) => {
-          return [rowIndex -1, rowIndex, rowIndex+1].includes(x) && [cellIndex -1, cellIndex, cellIndex +1].includes(y)
+        const filteredThing = mines.filter(([y, x]) => {
+          return (
+            [rowIndex - 1, rowIndex, rowIndex + 1].includes(x) &&
+            [cellIndex - 1, cellIndex, cellIndex + 1].includes(y)
+          );
         });
-        return filteredThing.length;
-      }
-    }));
+        return filteredThing.length.toString() as Field;
+      })
+    );
   }
-}
-
-enum Cell {
-  MINE = "*",
-  UNKOWN = ".",
 }
