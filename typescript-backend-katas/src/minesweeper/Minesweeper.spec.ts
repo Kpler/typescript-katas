0cd72fec12e60ad9184d3aa853e10fd88297af67
Grégoire Chauvet <gchauvet.home@gmail.com>
2022-09-04 18:33:45 +0200

    Play with win & lose

--- typescript-backend-katas/src/minesweeper/Minesweeper.spec.ts
+++ typescript-backend-katas/src/minesweeper/Minesweeper.spec.ts
@@ -17,4 +17,39 @@ describe("Minesweeper", () => {
 
     expect(game.toString()).toBe(expectedResult);
   });
+
+  it("should be able to play the game", () => {
+    const game = new Minesweeper(3, 3, [
+      [0, 1],
+      [1, 1],
+    ]);
+
+    expect(game.play([0, 0])).toBe("2..\n...\n...");
+    expect(game.play([0, 2])).toBe("2.2\n...\n...");
+  });
+
+  it("should lose when clicking on a bomb", () => {
+    const game = new Minesweeper(3, 3, [
+      [0, 1],
+      [1, 1],
+    ]);
+
+    expect(game.play([0, 1])).toBe("You lose");
+  });
+
+  it("should be able to win the game", () => {
+    const game = new Minesweeper(3, 3, [
+      [0, 1],
+      [1, 1],
+    ]);
+
+    game.play([0, 0]);
+    game.play([0, 2]);
+    game.play([1, 0]);
+    game.play([1, 2]);
+    game.play([2, 0]);
+    game.play([2, 1]);
+    const output = game.play([2, 2]);
+    expect(output).toBe("Win!");
+  });
 });
--- typescript-backend-katas/src/minesweeper/Minesweeper.ts
+++ typescript-backend-katas/src/minesweeper/Minesweeper.ts
@@ -7,10 +7,15 @@ type Cell = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "*";
 
 export class Minesweeper {
   #grid: Cell[][];
+  #reveal: boolean[][];
 
   constructor(rows: number, columns: number, mines: [number, number][]) {
     const gridWithMines = this.#generateMinefield(rows, columns, mines);
     this.#grid = this.#addCountsToMinefield(gridWithMines, mines);
+
+    this.#reveal = new Array(rows)
+      .fill(undefined)
+      .map(() => new Array(columns).fill(false));
   }
 
   #generateMinefield(
@@ -59,5 +64,45 @@ export class Minesweeper {
 
   toString(): string {
     return this.#grid.map((row) => row.join("")).join("\n");
+  }
+
+  #isWon(): boolean {
+    const safeCellCount = this.#grid
+      .flatMap((row, x) => {
+        return row.map((cell, y) => {
+          const isRevealed = this.#reveal[x][y];
+          const isBomb = cell === "*";
+          return { isRevealed, isBomb };
+        });
+      })
+      .filter(({ isRevealed, isBomb }) => {
+        return !isRevealed && !isBomb;
+      }).length;
+
+    return safeCellCount === 0;
+  }
+
+  play(coordinates: [number, number]): string {
+    const [x, y] = coordinates;
+    this.#reveal[x][y] = true;
+
+    const isBomb = this.#grid[x][y] === '*';
+    if (isBomb) {
+      return 'You lose';
+    }
+    if (this.#isWon()) {
+      return 'Win!'
+    }
+
+    return this.#grid
+      .map((row, gX) =>
+        row
+          .map((item, gY) => {
+            const isRevealed = this.#reveal[gX][gY];
+            return isRevealed ? item : ".";
+          })
+          .join("")
+      )
+      .join("\n");
   }
 }
