6a9e4de0f0f6df87e6c0b70d5b9ecea021ee5d0f
pguardati-kpler <98783908+pguardati-kpler@users.noreply.github.com>
2022-09-07 11:36:05 +0200

    refactor, start fix test

--- typescript-backend-katas/src/minesweeper/Minesweeper.spec.ts
+++ typescript-backend-katas/src/minesweeper/Minesweeper.spec.ts
@@ -1,39 +1,39 @@
-import { YouLoose, Minesweeper } from "./Minesweeper";
+import {YouLoose, Minesweeper} from "./Minesweeper";
 
 describe("Minesweeper", () => {
-  it("should generate game and display adjacent mines on empty minefield", () => {
-    const expectedResult = "000\n000\n000";
-    const game = new Minesweeper(3, 3, []);
+    it("should generate game and display adjacent mines on empty minefield", () => {
+        const expectedResult = "000\n000\n000";
+        const game = new Minesweeper(3, 3, []);
 
-    expect(game.toString()).toBe(expectedResult);
-  });
+        expect(game.toString()).toBe(expectedResult);
+    });
 
 
-  it("should display adjacent mines", () => {
-    const expectedResult = "2*2\n2*2\n111";
-    const game = new Minesweeper(3, 3, [
-      [0, 1],
-      [1, 1],
-    ]);
+    it("should display adjacent mines", () => {
+        const expectedResult = "2*2\n2*2\n111";
+        const game = new Minesweeper(3, 3, [
+            [0, 1],
+            [1, 1],
+        ]);
 
-    expect(game.toString()).toBe(expectedResult);
-  });
+        expect(game.toString()).toBe(expectedResult);
+    });
 
 //   get set coordinates, show number of near mines or tells about game over
 //   game over, winner
 //
 
-  it("should lose if current coordinates have a mine", () => {
-    const game = new Minesweeper(3, 3, [
-      [0, 0],
-    ]);
-    expect(() => game.play([0,0])).toThrowError(YouLoose);
-  });
+    it("should lose if current coordinates have a mine", () => {
+        const game = new Minesweeper(3, 3, [
+            [0, 0],
+        ]);
+        expect(() => game.play([0, 0])).toThrowError(YouLoose);
+    });
 
-  it("should continue if you are not on a mine and reveal adj number of mines", () => {
-    const game = new Minesweeper(3, 3, [
-      [0, 0],
-    ]);
-    expect(() => game.play([0,1])).toBe(1);
-  });
+    it("should continue if you are not on a mine and reveal adj number of mines", () => {
+        const game = new Minesweeper(3, 3, [
+            [0, 0],
+        ]);
+        expect(game.play([0, 1])).toBe(1);
+    });
 });
--- typescript-backend-katas/src/minesweeper/Minesweeper.ts
+++ typescript-backend-katas/src/minesweeper/Minesweeper.ts
@@ -1,72 +1,75 @@
 enum MineStatus {
-  MINE = "*",
-  CLEAR = ".",
+    MINE = "*",
+    CLEAR = ".",
 }
 
 type Cell = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "*";
 
-export class YouLoose extends Error {}
-export class YouWin extends Error {}
+export class YouLoose extends Error {
+}
+
+export class YouWin extends Error {
+}
 
 
 export class Minesweeper {
-  #grid: Cell[][];
+    #grid: Cell[][];
 
-  constructor(rows: number, columns: number, mines: [number, number][]) {
-    const gridWithMines = this.#generateMinefield(rows, columns, mines);
-    this.#grid = this.#addCountsToMinefield(gridWithMines, mines);
-  }
-
-  #generateMinefield(
-    rows: number,
-    columns: number,
-    mines: [number, number][]
-  ): MineStatus[][] {
-    const grid: MineStatus[][] = [];
-
-    for (let i = 0; i < rows; i++) {
-      const rowArr = [];
-      for (let j = 0; j < columns; j++) {
-        rowArr.push(MineStatus.CLEAR);
-      }
-      grid.push(rowArr);
+    constructor(rows: number, columns: number, mines: [number, number][]) {
+        const gridWithMines = this.#generateMinefield(rows, columns, mines);
+        this.#grid = this.#addCountsToMinefield(gridWithMines, mines);
     }
 
-    mines.forEach((mine) => {
-      const [mineX, mineY] = mine;
-      grid[mineX][mineY] = MineStatus.MINE;
-    });
+    #generateMinefield(
+        rows: number,
+        columns: number,
+        mines: [number, number][]
+    ): MineStatus[][] {
+        const grid: MineStatus[][] = [];
 
-    return grid;
-  }
-
-  #addCountsToMinefield(grid: MineStatus[][], mines: [number, number][]): Cell[][] {
-    return grid.map((row, rowIndex) =>
-      row.map((cell, cellIndex) => {
-        if (cell === MineStatus.MINE) {
-          return "*";
+        for (let i = 0; i < rows; i++) {
+            const rowArr = [];
+            for (let j = 0; j < columns; j++) {
+                rowArr.push(MineStatus.CLEAR);
+            }
+            grid.push(rowArr);
         }
 
-        const adjacentMines = mines.filter(([x, y]) => {
-          return (
-            [rowIndex - 1, rowIndex, rowIndex + 1].includes(x) &&
-            [cellIndex - 1, cellIndex, cellIndex + 1].includes(y)
-          );
+        mines.forEach((mine) => {
+            const [mineX, mineY] = mine;
+            grid[mineX][mineY] = MineStatus.MINE;
         });
-        return adjacentMines.length.toString() as Cell;
-      })
-    );
-  }
 
-  toString(): string {
-    return this.#grid
-      .map(row => row.join(""))
-      .join("\n");
-  }
+        return grid;
+    }
 
-  play([x, y]: [number, number]): number {
-    throw new YouLoose()
-    return 0;
-  }
+    #addCountsToMinefield(grid: MineStatus[][], mines: [number, number][]): Cell[][] {
+        return grid.map((row, rowIndex) =>
+            row.map((cell, cellIndex) => {
+                if (cell === MineStatus.MINE) {
+                    return "*";
+                }
+
+                const adjacentMines = mines.filter(([x, y]) => {
+                    return (
+                        [rowIndex - 1, rowIndex, rowIndex + 1].includes(x) &&
+                        [cellIndex - 1, cellIndex, cellIndex + 1].includes(y)
+                    );
+                });
+                return adjacentMines.length.toString() as Cell;
+            })
+        );
+    }
+
+    toString(): string {
+        return this.#grid
+            .map(row => row.join(""))
+            .join("\n");
+    }
+
+    play([x, y]: [number, number]): number {
+        throw new YouLoose()
+        return 0;
+    }
 }
 
