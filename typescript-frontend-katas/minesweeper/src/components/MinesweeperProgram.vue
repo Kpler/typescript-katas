5aff1ce77fa9920b1f45585a0d1148481f163be9
MOTRIEUX Thomas <tmotrieux@kpler.com>
2022-09-14 11:17:55 +0200

    WIP

--- typescript-frontend-katas/minesweeper/src/components/MinesweeperProgram.vue
+++ typescript-frontend-katas/minesweeper/src/components/MinesweeperProgram.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="minesweeper-program">
-    Put the Minesweeper game here
-  </div>
+  <p>{{ game.toString() }}</p>
 </template>
 
 <script lang="ts">
+import { Minesweeper } from "src/game/Minesweeper";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "MinesweeperProgram",
   setup() {
+    const mines: [number, number][] = [
+      [1, 1],
+      [3, 2],
+      [5, 6],
+    ];
+    const game = new Minesweeper(8, 8, mines);
 
+    return { game };
   },
 });
 </script>
