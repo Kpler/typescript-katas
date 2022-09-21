17e2e008de3bced0727c85231147afcc175e807f
Henry Nomichith <hnomichith@kpler.com>
2022-09-21 16:27:10 +0200

    add test templates

--- typescript-backend-katas/src/minesweeper/Minesweeper.spec.ts
+++ typescript-backend-katas/src/minesweeper/Minesweeper.spec.ts
@@ -1,6 +1,6 @@
 import { Minesweeper } from "./Minesweeper";
 
-describe("Minesweeper", () => {
+describe("constructor", () => {
   it("should generate game and display adjacent mines on empty minefield", () => {
     const expectedResult = "000\n000\n000";
     const game = new Minesweeper(3, 3, []);
@@ -16,5 +16,31 @@ describe("Minesweeper", () => {
     ]);
 
     expect(game.toString()).toBe(expectedResult);
+  });
+});
+
+describe("play", () => {
+  it("should display the cell played", () => {
+    // GIVEN
+    // WHEN
+    // THEN
+  });
+
+  it("should display the cells played when called several time", () => {
+    // GIVEN
+    // WHEN
+    // THEN
+  });
+
+  it("should return a game over when played on a mine", () => {
+    // GIVEN
+    // WHEN
+    // THEN
+  });
+
+  it("should display all adjacent cells without mines when played on a 0", () => {
+    // GIVEN
+    // WHEN
+    // THEN
   });
 });
