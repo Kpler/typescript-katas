561220494bc45463b0848d76958ea75e1a6e43f1
preetwinder <preetwinder14496@gmail.com>
2022-12-21 14:47:41 +0530

    feat: add alphabet constant

--- typescript-backend-katas/src/diamond/diamond.spec.ts
+++ typescript-backend-katas/src/diamond/diamond.spec.ts
@@ -1,7 +1,16 @@
 import { build_diamond } from './diamond';
 
+describe('get_alphabet_index', () => {
+    it('should return the index for an alphabet', () => {
+        expect(get_alphabet_index).toBe('A');
+    })
+})
+
 describe('diamond', () => {
     it('should build a diamond for letter A', () => {
         expect(build_diamond('A')).toBe('A');
+    })
+    it('should build a diamond for letter B', () => {
+        expect(build_diamond('B')).toBe(' A \nB B\n A ');
     })
 })
--- typescript-backend-katas/src/diamond/diamond.ts
+++ typescript-backend-katas/src/diamond/diamond.ts
@@ -1,3 +1,6 @@
+const ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
 export function build_diamond(letter: string): string {
     return 'A';
 }
