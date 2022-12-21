ae0f0d3f16ea325399b69d0432b8963bbe459515
antoine charlet <acharlet@kpler.com>
2022-12-21 10:30:32 +0100

    antoine

--- typescript-backend-katas/src/diamond/diamond.spec.ts
+++ typescript-backend-katas/src/diamond/diamond.spec.ts
@@ -1,8 +1,9 @@
-import { build_diamond } from './diamond';
+import { build_diamond, get_slice } from './diamond';
 
 describe('get_alphabet_index', () => {
-    it('should return the index for an alphabet', () => {
-        expect(get_alphabet_index).toBe('A');
+    it('should return the first slice  of the alphabet for a letter', () => {
+        expect(get_slice('C')).toBe('ABC');
+        expect(get_slice('D')).toBe('ABCD');
     })
 })
 
@@ -12,5 +13,11 @@ describe('diamond', () => {
     })
     it('should build a diamond for letter B', () => {
         expect(build_diamond('B')).toBe(' A \nB B\n A ');
+    })
+})
+
+describe('build_row', () => {
+    it('should build a row for letter B', () => {
+        expect(build_row('B')).toBe('B B');
     })
 })
--- typescript-backend-katas/src/diamond/diamond.ts
+++ typescript-backend-katas/src/diamond/diamond.ts
@@ -2,5 +2,24 @@ const ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
 export function build_diamond(letter: string): string {
-    return 'A';
+    const letters = get_slice(letter)
+    row = ""
+    for (let i = 0; i < letters.length; i++) {
+        row
+    }
+
+}
+
+export function get_slice(letter: string): string {
+    const index = ALPHABETS.indexOf(letter)
+    return ALPHABETS.slice(0, index + 1)
+}
+
+export function build_row(letter: string, max_size): string {
+    const letters = get_slice(letter)
+    row = ""
+    for (let i = 0; i < letters.length; i++) {
+        row
+    }
+
 }
