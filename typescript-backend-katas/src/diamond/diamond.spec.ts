2e2acdd963cd8aca9060431d35a92d067b9c764a
Nicolas Guignard <nicolas065@gmail.com>
2022-12-21 10:06:19 +0100

    first WIP test

---
+++ typescript-backend-katas/src/diamond/diamond.spec.ts
@@ -0,0 +1,37 @@
+describe("Compute diamond", () => {
+    it("should return the list of strings displayed for the simplest character - A", () => {
+        // const result = initCalc();
+        expect(result).toBe(["A"]);
+    });
+});
+/*
+Input A:
+Output: [A]
+
+Input: [A]
+Output:
+A
+=======
+Input A:
+Output:
+A
+
+---------
+Input A:
+Output: [A, BB, A]
+
+Input: [A, BB, A]
+Input B:
+ A
+B B
+ A
+
+
+ ========
+
+ Input B:
+Output:
+ A
+B B
+ A
+ */
