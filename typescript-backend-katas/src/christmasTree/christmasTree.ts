b2ab96d0fc1ebede10a79946a5b7999f63798874
ejaoua <ejaoua@kpler.com>
2022-12-22 17:26:06 +0100

    add implem smallTree

--- typescript-backend-katas/src/christmasTree/christmasTree.ts
+++ typescript-backend-katas/src/christmasTree/christmasTree.ts
@@ -5,6 +5,16 @@ export const getChristmasTree = (height: number): string[] => {
       "|",
     ];
   }
+  const smallTree = getChristmasTree(height-1)
   
-  return getChristmasTree(height-1)
+  return smallTree.forEach((stage, stageIndex) => {
+      if (stageIndex === 0) {
+        stage.unshift(" ");
+        stage.push(" ");
+      }
+      else {
+        stage.unshift("X");
+        stage.push("X");
+      }
+  })
 }
