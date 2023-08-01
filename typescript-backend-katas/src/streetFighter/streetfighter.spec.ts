3601130736056a089d9b6cf9d4ca05e39ca4b71f
bgigodeaux <baptistegigodeaux@gmail.com>
2023-08-01 15:46:37 +0200

    feat: iteration 2

--- typescript-backend-katas/src/streetFighter/streetfighter.spec.ts
+++ typescript-backend-katas/src/streetFighter/streetfighter.spec.ts
@@ -1,10 +1,10 @@
 import { rank } from "./streetfighter";
-import {calculate} from "../calculator/calculator.level2";
+import { calculate } from "../calculator/calculator.level2";
 
 
 describe("Streetfighter", () => {
     it("should run", () => {
-        expect(rank()).toBe(Array());
+        expect(rank()).toStrictEqual([]);
     });
 });
 
--- typescript-backend-katas/src/streetFighter/streetfighter.ts
+++ typescript-backend-katas/src/streetFighter/streetfighter.ts
@@ -1,5 +1,5 @@
 
 
 export const rank = (): Array<string> => {
-    return Array()
+    return []
 }
