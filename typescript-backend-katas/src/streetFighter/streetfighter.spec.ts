93356c5939931168c2e2b2d986c77be9b11b2dbe
JulienPiquot <jpiquot@kpler.com>
2023-08-01 15:40:24 +0200

    first commit

---
+++ typescript-backend-katas/src/streetFighter/streetfighter.spec.ts
@@ -0,0 +1,12 @@
+import { rank } from "./streetfighter";
+import {calculate} from "../calculator/calculator.level2";
+
+
+describe("Streetfighter", () => {
+    it("should run", () => {
+        expect(rank()).toBe(Array());
+    });
+});
+
+
+
