75830959fb24d8dab8a62f3ba1b5266a8126745f
Nicolas Guignard <nicolas065@gmail.com>
2023-08-11 11:36:51 +0200

    WIP

--- typescript-backend-katas/src/diamond/diamond.ts
+++ typescript-backend-katas/src/diamond/diamond.ts
@@ -2,28 +2,30 @@ export function computeDiamond(diamondVariable: string): Array<String> {
     
     const letters: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    /*const charMap = {
-        'a': ['a'],
-        'b': ['*a', 'b*b', '*a']
-    }*/
-
-    /*
-     Idea: get the difference between character a and the requested one (diamondVariable)
-     to calculate the number of stars
-     */
     const result: string[] = [];
     let tempChar = '';
-    for(const letter in letters) {
-        tempChar = '';
-        const diffChar = letters.indexOf(letter);
+
+    function computeStars(diffChar: number) {
         for (let i = 0; i < diffChar; i++) {
             tempChar = tempChar.concat('*');
+        }
+    }
+
+    for(const letter of letters) {
+        tempChar = '';
+        const diffChar = letters.indexOf(diamondVariable) - letters.indexOf(letter);
+        if (letter !== diamondVariable) {
+            computeStars(diffChar);
         }
         tempChar = tempChar.concat(letter);
         console.log(tempChar);
 
-        if (diamondVariable !== 'a') {
-            tempChar = tempChar.concat(tempChar);
+        if (letter !== 'a') {
+
+            computeStars(diffChar);
+            tempChar = tempChar.concat(letter);
+            result.push(tempChar);
+        } else {
             result.push(tempChar);
         }
         if (letter == diamondVariable) {
@@ -32,6 +34,5 @@ export function computeDiamond(diamondVariable: string): Array<String> {
 
     }
 
-    result.unshift(tempChar);
     return result;
 }
