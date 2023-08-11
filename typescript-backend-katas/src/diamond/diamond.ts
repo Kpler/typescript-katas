12bf48a21f1eff4c8c9532a43d58c11454e4ccec
Nicolas Guignard <nicolas065@gmail.com>
2023-08-11 10:38:03 +0200

    make b test pass

--- typescript-backend-katas/src/diamond/diamond.ts
+++ typescript-backend-katas/src/diamond/diamond.ts
@@ -8,9 +8,22 @@ export function computeDiamond(diamondVariable: string): Array<String> {
      Idea: get the difference between character a and the requested one (diamondVariable)
      to calculate the number of stars
      */
-    const count = "b".charCodeAt(0);
-    const diffChar = diamondVariable.charCodeAt(0) - "a".charCodeAt(0)
-    console.log(count);
-
-    return [diamondVariable]
+    const diffChar = diamondVariable.charCodeAt(0) - "a".charCodeAt(0);
+    const result: string[] = [];
+    let aChar = ''
+    for (let i = 0; i < diffChar; i++) {
+        aChar = aChar.concat('*');
+    }
+    aChar = aChar.concat('a');
+    result.push(aChar);
+    if (diamondVariable !== 'a') {
+        let bChar = 'b';
+        for (let i = 0; i < diffChar; i++) {
+            bChar = bChar.concat('*');
+        }
+        bChar = bChar.concat('b');
+        result.push(bChar);
+        result.push(aChar);
+    }
+    return result;
 }
