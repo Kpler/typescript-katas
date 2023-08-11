b18c879521f9a65aafdb5d30bba944d8824b476c
Daniel Merkel <merkel@fleetmon.com>
2023-08-11 10:28:54 +0200

    feat(diament): try calculation of characters

--- typescript-backend-katas/src/diamond/diamond.spec.ts
+++ typescript-backend-katas/src/diamond/diamond.spec.ts
@@ -1,12 +1,12 @@
 import {computeDiamond} from "./diamond"
 
 describe("computeDiamond", () => {
-  it("shoud return a", () => {
+  it("should return a", () => {
     const diamond = computeDiamond('a');
     expect(diamond).toEqual(['a']);
   });
 
-  it("shoud return diamond with b ", () => {
+  it("should return diamond with b ", () => {
     const diamond = computeDiamond('b');
     expect(diamond).toEqual(['*a','b*b','*a']);
   });
--- typescript-backend-katas/src/diamond/diamond.ts
+++ typescript-backend-katas/src/diamond/diamond.ts
@@ -1,3 +1,16 @@
 export function computeDiamond(diamondVariable: string): Array<String> {
-    return ['a']
+    /*const charMap = {
+        'a': ['a'],
+        'b': ['*a', 'b*b', '*a']
+    }*/
+
+    /*
+     Idea: get the difference between character a and the requested one (diamondVariable)
+     to calculate the number of stars
+     */
+    const count = "b".charCodeAt(0);
+    const diffChar = diamondVariable.charCodeAt(0) - "a".charCodeAt(0)
+    console.log(count);
+
+    return [diamondVariable]
 }
