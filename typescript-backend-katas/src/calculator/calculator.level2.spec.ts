dfabd70e35a0d921dfd147a08574b6974ce5eee9
DavidPalmi <92101787+DavidPalmi@users.noreply.github.com>
2023-08-24 09:46:14 -0400

    refactor: refactor

--- typescript-backend-katas/src/calculator/calculator.level2.spec.ts
+++ typescript-backend-katas/src/calculator/calculator.level2.spec.ts
@@ -6,8 +6,9 @@ fdescribe("Calculator", () => {
     expect(calculate("4 + 8 + 15 + 16 + 23 + 42")).toBe(108);
   });
 
-  it("should execute substractions correctly", () => {
-    expect(calculate("5 - 3")).toBe(2);
+  it("should execute subtractions correctly", () => {
+    expect(calculate("5 - 3")).toBe(2);    
+    expect(calculate("5 - -3")).toBe(8);
   });
 
 });
--- typescript-backend-katas/src/calculator/calculator.level2.ts
+++ typescript-backend-katas/src/calculator/calculator.level2.ts
@@ -1,16 +1,21 @@
+enum Operators{
+  Addition = '+',
+  Subtraction = '-'
+}
+
 export const calculate = (expression: string): number => {
     let result = 0;
-    let nextOperation= '+';
+    let nextOperation = Operators.Addition;
     expression.split(" ").forEach(
         (curStr) => {
-            if (!isNaN(Number(curStr)) && nextOperation === '+') {
+            if (!isNaN(Number(curStr)) && nextOperation === Operators.Addition) {
                 result += Number(curStr);
-            } else if (!isNaN(Number(curStr)) && nextOperation === '-') {
+            } else if (!isNaN(Number(curStr)) && nextOperation === Operators.Subtraction) {
                 result -= Number(curStr);
-            } else if (curStr === '-') {
-                nextOperation = '-';
-            } else if (curStr === '+') {
-                nextOperation = '+';
+            } else if (curStr === Operators.Subtraction) {
+                nextOperation = Operators.Subtraction;
+            } else if (curStr === Operators.Addition) {
+                nextOperation = Operators.Addition;
             }
         }
     )
