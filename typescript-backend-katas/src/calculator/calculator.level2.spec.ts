fb43ec3f747524633cafa5ab87f0d64f501af542
ejaoua <ejaoua@kpler.com>
2023-08-24 15:21:37 +0200

    feat(calculator): add operator

--- typescript-backend-katas/src/calculator/calculator.level2.spec.ts
+++ typescript-backend-katas/src/calculator/calculator.level2.spec.ts
@@ -6,4 +6,8 @@ fdescribe("Calculator", () => {
     expect(calculate("4 + 8 + 15 + 16 + 23 + 42")).toBe(108);
   });
 
+  it("should execute substractions correctly", () => {
+    expect(calculate("5 - 3")).toBe(2);
+  });
+
 });
--- typescript-backend-katas/src/calculator/calculator.level2.ts
+++ typescript-backend-katas/src/calculator/calculator.level2.ts
@@ -1,9 +1,16 @@
 export const calculate = (expression: string): number => {
     let result = 0;
+    let nextOperation= '+';
     expression.split(" ").forEach(
-        (curChar, i) => {
-            if (!isNaN(Number(curChar))) {
-                result += Number(curChar);
+        (curStr) => {
+            if (!isNaN(Number(curStr))) {
+                result += Number(curStr);
+            }
+            if (curStr === '-') {
+              nextOperation = '-';
+            }
+            if (curStr === '+') {
+              nextOperation = '+';
             }
         }
     )
