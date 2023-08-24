7431cd02e2df3a1920489a9fdbdc103e0e53c36f
Nicolas Guignard <nicolas065@gmail.com>
2023-08-24 14:40:02 +0200

    init

--- typescript-backend-katas/src/calculator/calculator.level2.ts
+++ typescript-backend-katas/src/calculator/calculator.level2.ts
@@ -1,29 +1,3 @@
-enum Operators {
-  ADD = "+",
-  SUBSTRACT = "-",
-  MULTIPLY = "*",
-  DIVIDE = "/",
-}
-
-const operators_to_method: { [key: string]: (a: number, b: number) => number} = {
-  [Operators.MULTIPLY]: (a: number, b: number) => a*b,
-  [Operators.DIVIDE]: (a: number, b: number) => a/b,
-  [Operators.ADD]: (a: number, b: number) => a+b,
-  [Operators.SUBSTRACT]: (a: number, b: number) => a-b,
-}
-
-const operators_by_reversed_priority = ["+", "-", "/", "*"]
-
 export const calculate = (expression: string): number => {
-  const expression_as_list = expression.split(" ");
-
-  for (let operator of operators_by_reversed_priority) {
-    if (!expression_as_list.includes(operator)) {
-      continue;
-    }
-    const [left, ...right] = expression.split(` ${operator} `);
-    return operators_to_method[operator](calculate(left), calculate(right.join(` ${operator} `)))
-  }
-
-  return Number(expression);
+  return 0;
 }
