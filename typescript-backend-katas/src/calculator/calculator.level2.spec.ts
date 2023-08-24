176a582b8156b78feba2d1b153123a79241009bb
Ben Janowski <janowski@fleetmon.com>
2023-08-24 14:58:19 +0200

    fix: working 1st test

--- typescript-backend-katas/src/calculator/calculator.level2.spec.ts
+++ typescript-backend-katas/src/calculator/calculator.level2.spec.ts
@@ -1,6 +1,6 @@
 import { calculate } from "./calculator.level2";
 
-describe("Calculator", () => {
+fdescribe("Calculator", () => {
   it("should execute additions correctly", () => {
     expect(calculate("1 + 2")).toBe(3);
     expect(calculate("4 + 8 + 15 + 16 + 23 + 42")).toBe(108);
--- typescript-backend-katas/src/calculator/calculator.level2.ts
+++ typescript-backend-katas/src/calculator/calculator.level2.ts
@@ -1,9 +1,13 @@
 export const calculate = (expression: string): number => {
   let result = 0;
   for (let i = 0; i < expression.length; i++) {
+    let temp = expression.charAt(i);
     if (!isNaN(Number(expression.charAt(i)))) {
-      let temp = expression.charAt(i);
-      result += Number(expression.charAt(i));
+      while (!isNaN(Number(expression.charAt(i + 1))) && i < expression.length) {
+        temp += expression.charAt(i + 1);
+        i++;
+      }
+      result += Number(temp);
     }
   }
   return result;
