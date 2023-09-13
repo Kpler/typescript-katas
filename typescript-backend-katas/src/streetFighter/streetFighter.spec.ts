a71c96b884a9bc10ade67c834f6e881d42042da6
DavidPalmi <92101787+DavidPalmi@users.noreply.github.com>
2023-09-13 10:07:16 -0400

    feat: add type to csv parser

--- typescript-backend-katas/src/streetFighter/streetFighter.spec.ts
+++ typescript-backend-katas/src/streetFighter/streetFighter.spec.ts
@@ -1,23 +1,25 @@
 import { computeMatchResults, PlayerResult } from "./streetFighter";
 
 describe("Given a match between chun-li and sagat won 2 rounds to 1", () => {
-  it("should return the 2 players in a list with chun-li having 3 points and sagat having 0 pts", () => {
+  it("should return the 2 players in a list with chun-li having 3 points and sagat having 0 pts", async () => {
     const expectedResult = [
       new PlayerResult("chun-li", 3, 1),
       new PlayerResult("sagat", 0, 2),
     ];
-    const result = computeMatchResults('src/streetFighter/helpers/testResources/one-match.csv');
+    const result = await computeMatchResults('src/streetFighter/helpers/testResources/one-match.csv');
     expect(result).toStrictEqual(expectedResult);
   });
 });
 
 describe("Given of list of two matches between chun-li and sagat and chun-li and zangief", () => {
-  const expectedResult = [
-    new PlayerResult("chun-li", 3, 1),
-    new PlayerResult("sagat", 0, 2),
-    new PlayerResult("zangief", 0, 2),
-  ];
+  it("should return the 2 players in a list with chun-li having 3 points and sagat and zangief having 0 pts", async () => {
+    const expectedResult = [
+      new PlayerResult("chun-li", 3, 1),
+      new PlayerResult("sagat", 0, 2),
+      new PlayerResult("zangief", 0, 2),
+    ];
 
-  const result = computeMatchResults('src/streetFighter/helpers/testResources/two-matches.csv');
-  expect(result).toStrictEqual(expectedResult);
+    const result = await computeMatchResults('src/streetFighter/helpers/testResources/two-matches.csv');
+    expect(result).toStrictEqual(expectedResult);
+  });
 });
--- typescript-backend-katas/src/streetFighter/streetFighter.ts
+++ typescript-backend-katas/src/streetFighter/streetFighter.ts
@@ -4,7 +4,13 @@ export class PlayerResult {
     constructor(public name: string, public points: number, public roundsWon: number) { }
 }
 
-export function computeMatchResults(fileName: string) {
-    const matches = await parseCsvFile(fileName, ',');
+export async function computeMatchResults(fileName: string) {
+    const matches = await parseCsvFile<{
+      timeslot: number,
+      home: string,
+      roundsWon1: number,
+      roundsWon2: number,
+      away: string
+    }>(fileName, ',');
     return [new PlayerResult("chun-li", 3, 1), new PlayerResult("sagat", 0, 2)];
 }
