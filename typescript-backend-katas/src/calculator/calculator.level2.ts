9f9cc422fc11c5930d0f40b758d12f48d8abe69f
Nicolas Guignard <nicolas065@gmail.com>
2023-10-04 16:05:15 +0200

    make addition and subtraction work

--- typescript-backend-katas/src/calculator/calculator.level2.ts
+++ typescript-backend-katas/src/calculator/calculator.level2.ts
@@ -1,34 +1,31 @@
 enum Operation {
-  ADDITION = '+',
-  SUBTRACTION = '-',
+    ADDITION = '+',
+    SUBTRACTION = '-',
 }
 
 export const calculate = (expression: string): number => {
-  const expressionElements: string[] = expression.split(' ');
+    const expressionElements: string[] = expression.split(' ');
 
-  let result = 0;
-  let nextOperation: Operation = Operation.ADDITION;
+    let result = 0;
+    let nextOperation: (x: number, y: number) => number = (x, y) => add(x,  y);
 
-  expressionElements.forEach(element => {
-    if (isOperand(element)) {
-      if (nextOperation===Operation.ADDITION) {
-        result += Number(element);
-      }
-      else {
-        result -= Number(element);
-      }
-    }
-    else {
-      if (element === Operation.ADDITION) {
-        nextOperation = Operation.ADDITION;
-      } else {
-        nextOperation = Operation.SUBTRACTION;
-      }
-    }
-  });
+    expressionElements.forEach(element => {
+        if (isOperand(element)) {
+            result = nextOperation(result, Number(element));
+        } else {
+            if (element === Operation.ADDITION) {
+                nextOperation = add;
+            } else {
+                nextOperation = subtract;
+            }
+        }
+    });
 
-  return result;
+    return result;
 }
 const isOperand = (element: string): boolean => {
-  return element !== Operation.ADDITION && element !== Operation.SUBTRACTION;
+    return element !== Operation.ADDITION && element !== Operation.SUBTRACTION;
 }
+
+const add: (x: number, y: number) => number = (x, y) => x + y;
+const subtract: (x: number, y: number) => number = (x, y) => x - y;
