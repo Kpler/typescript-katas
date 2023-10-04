2bf11de489dd42de8e241655907cfe24ec9705e6
Jan Warnecke-Hasselberg <jhasselberg@kpler.com>
2023-10-04 15:54:55 +0200

    WIP

--- typescript-backend-katas/src/calculator/calculator.level2.ts
+++ typescript-backend-katas/src/calculator/calculator.level2.ts
@@ -1,17 +1,34 @@
+enum Operation {
+  ADDITION = '+',
+  SUBTRACTION = '-',
+}
+
 export const calculate = (expression: string): number => {
   const expressionElements: string[] = expression.split(' ');
 
   let result = 0;
-  let nextOperation: '+' | '-';
+  let nextOperation: Operation = Operation.ADDITION;
 
   expressionElements.forEach(element => {
     if (isOperand(element)) {
-      result += Number(element);
+      if (nextOperation===Operation.ADDITION) {
+        result += Number(element);
+      }
+      else {
+        result -= Number(element);
+      }
+    }
+    else {
+      if (element === Operation.ADDITION) {
+        nextOperation = Operation.ADDITION;
+      } else {
+        nextOperation = Operation.SUBTRACTION;
+      }
     }
   });
 
   return result;
 }
 const isOperand = (element: string): boolean => {
-  return element !== '+';
+  return element !== Operation.ADDITION && element !== Operation.SUBTRACTION;
 }
