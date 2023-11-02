05cbe6103e8348ad988dd1761f6815f9815d2a80
Ben Janowski <janowski@fleetmon.com>
2023-11-02 16:45:43 +0100

    fix: added constructor WIP

--- typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
@@ -1,29 +1,60 @@
-import { PasswordValidator, Validator, VALIDATORS } from "./passwordValidation";
+import { PasswordValidator, VALIDATORS } from "./passwordValidation";
 
-const validator  = new PasswordValidator(VALIDATORS[0]);
-
-describe("Password", () => {
+describe("Validator 1: Password", () => {
     it("should have at least 8 characters", () => {
+        const validator  = new PasswordValidator(VALIDATORS[0]);
         expect(validator.validate('Ae_12345')).toBe(true);
     });
 
     it("should not have less than 8 characters", () => {
+        const validator  = new PasswordValidator(VALIDATORS[0]);
         expect(validator.validate('Ae_1234')).toBe(false);
     });
 
     it("should contain at least 1 capital letter", () => {
+        const validator  = new PasswordValidator(VALIDATORS[0]);
         expect(validator.validate('ae_12345')).toBe(false);
     });
 
     it("should contain at least 1 lower case letter", () => {
+        const validator  = new PasswordValidator(VALIDATORS[0]);
         expect(validator.validate('AE_12345')).toBe(false);
     });
 
     it("should contain at least 1 number", () => {
+        const validator  = new PasswordValidator(VALIDATORS[0]);
         expect(validator.validate('AE_qwertyuiop')).toBe(false);
     });
 
     it("should contain at least 1 underscore", () => {
+        const validator  = new PasswordValidator(VALIDATORS[0]);
         expect(validator.validate('AEq123456')).toBe(false);
+    });
+});
+
+describe("Validator 2: Password", () => {
+    it("should have at least 6 characters", () => {
+        const validator  = new PasswordValidator(VALIDATORS[1]);
+        expect(validator.validate2('Ae1234')).toBe(true);
+    });
+
+    it("should not have less than 6 characters", () => {
+        const validator  = new PasswordValidator(VALIDATORS[1]);
+        expect(validator.validate2('Ae123')).toBe(false);
+    });
+
+    it("should contain at least 1 capital letter", () => {
+        const validator  = new PasswordValidator(VALIDATORS[1]);
+        expect(validator.validate2('ae_12345')).toBe(false);
+    });
+
+    it("should contain at least 1 lower case letter", () => {
+        const validator  = new PasswordValidator(VALIDATORS[1]);
+        expect(validator.validate2('AE_12345')).toBe(false);
+    });
+
+    it("should contain at least 1 number", () => {
+        const validator  = new PasswordValidator(VALIDATORS[1]);
+        expect(validator.validate2('AE_qwertyuiop')).toBe(false);
     });
 });
--- typescript-backend-katas/src/passwordValidation/passwordValidation.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.ts
@@ -1,11 +1,24 @@
 export class PasswordValidator {
+    private validator: Validator;
 
-    public validate(password: string): boolean {
-        return this.isLongEnough(password) && this.validateCase(password) && this.containsNumber(password) && this.containsUnderscore(password);
+    constructor(validator: Validator) {
+        this.validator = validator;
     }
 
-    private isLongEnough(password: string): boolean {
+    public validate(password: string): boolean {
+        return this.isAtLeastEightCharactersLong(password) && this.validateCase(password) && this.containsNumber(password) && this.containsUnderscore(password);
+    }
+
+    public validate2(password: string): boolean {
+        return this.isAtLeastSixCharactersLong(password) && this.validateCase(password) && this.containsNumber(password);
+    }
+
+    private isAtLeastEightCharactersLong(password: string): boolean {
         return password.length >= 8;
+    }
+
+    private isAtLeastSixCharactersLong(password: string): boolean {
+        return password.length >= 6;
     }
 
     private validateCase(password: string): boolean {
@@ -21,5 +34,5 @@ export class PasswordValidator {
     }
 }
 
-export const VALIDATORS = ['1', '2', '3', '4'] as const
+export const VALIDATORS = ['Validator1', 'Validator2', 'Validator3', 'Validator4'] as const
 export type Validator = typeof VALIDATORS[number]
