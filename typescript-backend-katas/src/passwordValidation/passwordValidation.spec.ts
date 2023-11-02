7e3a79a408323bcef975ae703668a6f0b548bac0
Ben Janowski <janowski@fleetmon.com>
2023-11-02 15:33:07 +0100

    feat: added new tests

--- typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
@@ -1,7 +1,15 @@
-import {passwordValidator} from "./passwordValidation";
+import { passwordValidator } from "./passwordValidation";
 
 describe("Password", () => {
     it("should have at least 8 characters", () => {
-        expect(passwordValidator('12345678')).toBe(true);
+        expect(passwordValidator('Ae_12345')).toBe(true);
+    });
+
+    it("should not have less than 8 characters", () => {
+        expect(passwordValidator('Ae_1234')).toBe(false);
+    });
+
+    it("should contain atleast 1 capital letter", () => {
+        expect(passwordValidator('ae_12345')).toBe(false);
     });
 });
--- typescript-backend-katas/src/passwordValidation/passwordValidation.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.ts
@@ -1,3 +1,3 @@
-export function passwordValidator(_: string): boolean {
-    return true;
+export function passwordValidator (password: string): boolean {
+    return password.length >= 8 && /[A-Z]/.test(password);
 }
