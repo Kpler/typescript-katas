d59ffd5183e9771514b8939e533af866701e2301
Jan Warnecke-Hasselberg <jhasselberg@kpler.com>
2023-12-05 17:01:19 +0100

    refactor: mappedRulesets

--- typescript-backend-katas/src/passwordValidation/passwordValidation.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.ts
@@ -2,6 +2,10 @@ const isNotLongEnough = (password: string): boolean => {
     return password.length <= 8;
 }
 
+const isNotLongEnoughTwo = (password: string): boolean => {
+    return password.length <= 6;
+}
+
 const isMissingCapitalLetter = (password: string): boolean => {
     return password === password.toLocaleLowerCase();
 }
@@ -28,6 +32,12 @@ const rules: Array<(password: string) => boolean> = [
 
 type VALIDATION = 'VALIDATION_1' | 'VALIDATION_2';
 
+const mappedRulesets = new Map([[ 'VALIDATION_1', rules ], [ 'VALIDATION_2', [isNotLongEnoughTwo] ]]);
+
 export const isPasswordValid = (password: string, validationType: VALIDATION = 'VALIDATION_1'): boolean => {
-    return !rules.some((rule) => rule(password));
+    const ruleset = mappedRulesets.get(validationType);
+    if (!ruleset) {
+        throw new Error(`Validation type ${validationType} is not supported`);
+    }
+    return !ruleset.some((rule) => rule(password));
 }
