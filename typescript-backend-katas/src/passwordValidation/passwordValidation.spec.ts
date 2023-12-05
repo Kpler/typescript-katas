b3270b1bbd31eaa1318489d07958fd8f889d2e9e
Ben Janowski <janowski@fleetmon.com>
2023-12-05 15:47:39 +0100

    feat: added more tests

--- typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
@@ -1,23 +1,23 @@
 import { isPasswordValid } from "./passwordValidation";
 
-
-describe("Given the password 'Kpler4_3ver'", () => {
-    it("should return that the password is valid", () => {
+describe("Is Password Valid", () => {
+    test("Given the password 'Kpler4_3ver' should return that the password is valid", () => {
         const password = 'Kpler4_3ver';
         expect(isPasswordValid(password)).toBe(true);
     });
-});
 
-describe("Given the password 'Kpler_2'", () => {
-    it("should return that the password is not valid", () => {
+    test("Given the password 7 characters should return that the password is not valid", () => {
         const password = 'Kpler_2';
         expect(isPasswordValid(password)).toBe(false);
     });
-});
 
-describe("Given the password 'Kpler43_'", () => {
-    it("should return that the password is invalid", () => {
+    test("Given the password 8 characters should return that the password is invalid", () => {
         const password = 'Kpler43_';
         expect(isPasswordValid(password)).toBe(false);
     });
-});
+
+    test("Given the password missing a capital letter should return that the password is invalid", () => {
+        const password = 'kpler443_';
+        expect(isPasswordValid(password)).toBe(false);
+    });
+})
--- typescript-backend-katas/src/passwordValidation/passwordValidation.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.ts
@@ -1,10 +1,14 @@
-export const isLongEnough = (password: string) : boolean => {
-    return password.length > 8;
+export const isNotLongEnough = (password: string) : boolean => {
+    return password.length <= 8;
+}
+
+export const isMissingCapitalLetter = (password: string) : boolean => {
+    
 }
 
 
 export const isPasswordValid = (password: string) : boolean => {
-    if (isLongEnough(password)) {
+    if (isNotLongEnough(password)) {
         return false;
     }
     return true;
