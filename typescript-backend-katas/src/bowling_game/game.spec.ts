bf6252557b178c0eeabd36f3b5c7d11ee8eca593
Nicolas Guignard <nicolas065@gmail.com>
2024-02-09 15:24:19 +0100

    feat: initialise game

--- typescript-backend-katas/src/bowling_game/game.spec.ts
+++ typescript-backend-katas/src/bowling_game/game.spec.ts
@@ -1,5 +1,13 @@
-describe("Example test", () => {
-    it("should sum numbers correctly", () => {
-        expect(1 + 1).toBe(2);
+import {Game} from "./game";
+
+describe("when playing bowling", () => {
+    it("we should initialise the score at the start", () => {
+        const game = new Game();
+        expect(game.score()).toBe(0);
+    });
+    it("we should increase score when playing once", () => {
+        const game = new Game();
+        game.roll(1)
+        expect(game.score()).toBe(1);
     });
 });
---
+++ typescript-backend-katas/src/bowling_game/game.ts
@@ -0,0 +1,5 @@
+export class Game {
+    score(): number {
+        return 0;
+    }
+}
