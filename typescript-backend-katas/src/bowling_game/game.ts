317b8d590d402abbc82a58dbef02b8cea926bd75
manolisan <manastasiou@kpler.com>
2024-02-09 16:54:19 +0200

    add bowling frames

--- typescript-backend-katas/src/bowling_game/game.ts
+++ typescript-backend-katas/src/bowling_game/game.ts
@@ -1,6 +1,6 @@
 export class Game {
     #currentScore: number = 0;
-    #frames: Array<{roll1: number, roll2: number}> = [];
+    #frames: Array<{roll1: number, roll2?: number}> = [];
 
     score(): number {
         return this.#currentScore;
@@ -8,8 +8,10 @@ export class Game {
 
     roll(number: number) {
         const currentFrame = this.#frames[this.#frames.length - 1];
-        
-
-        this.#currentScore += number;
+        if (currentFrame.roll2 === undefined) {
+            currentFrame.roll2 = number
+        } else {
+            this.#frames.push({roll1: number, roll2: undefined})
+        }
     }
 }
