159685746da4ca8b16a11d5d872062bc75047c92
Simon Boeuf <sboeuf@ekinox.io>
2024-02-09 15:45:50 +0100

    yet another test

--- typescript-backend-katas/src/bowling_game/game.spec.ts
+++ typescript-backend-katas/src/bowling_game/game.spec.ts
@@ -1,13 +1,25 @@
 import {Game} from "./game";
 
 describe("when playing bowling", () => {
+    let game: Game;
+    beforeEach(() => {
+        game = new Game();
+    });
+
     it("we should initialise the score at the start", () => {
-        const game = new Game();
         expect(game.score()).toBe(0);
     });
+
     it("we should increase score when playing once", () => {
-        const game = new Game();
         game.roll(1)
         expect(game.score()).toBe(1);
+    });
+
+    it("when somebody knocks down 5 then 5 then 3 the score should be 16", () => {
+        game.roll(5);
+        game.roll(5);
+        game.roll(3);
+        
+        expect(game.score()).toBe(16);
     });
 });
--- typescript-backend-katas/src/bowling_game/game.ts
+++ typescript-backend-katas/src/bowling_game/game.ts
@@ -1,11 +1,15 @@
 export class Game {
     #currentScore: number = 0;
+    #frames: Array<{roll1: number, roll2: number}> = [];
 
     score(): number {
         return this.#currentScore;
     }
 
     roll(number: number) {
+        const currentFrame = this.#frames[this.#frames.length - 1];
+        
+
         this.#currentScore += number;
     }
 }
