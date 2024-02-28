385df5f404b949d81da815fec59ed1eb17ad5e34
Nicolas Guignard <nicolas065@gmail.com>
2024-02-28 18:09:36 +0100

    feat: latest version

--- typescript-backend-katas/src/passwordValidation/passwordValidation.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.ts
@@ -14,22 +14,36 @@ const containsAnUnderscore = (password: string) => password.indexOf('_') >= 0
 
 const containsALowercase = (password: string) => /[a-z]/.test(password)
 
-export interface IPasswordValidator {
-  isValid(password: string): boolean;
+abstract class IPasswordValidator {
+    public rules: ((password: string) => boolean)[]
+
+    constructor(rules: ((password: string) => boolean)[]) {
+        this.rules = rules;
+    }
+
+    isValid(password: string): boolean {
+        return this.rules.every((rule) => rule(password))
+    }
 }
 
-export class PasswordValidator implements PasswordValidatorInterface{
-
-  public isValid(password: string): boolean {
-    return isPasswordValid(password);
-  }
-
+export class PasswordValidator extends IPasswordValidator {
+    constructor() {
+        const rules = [
+            isPasswordLongEnough,
+            containsALowercase,
+            containsANumber,
+            containsAnUnderscore,
+            containsAnUppercase
+        ]
+        super(rules);
+    }
 }
 
-export class PasswordValidatorTwo implements PasswordValidatorInterface{
-
-  public isValid(password: string): boolean {
-    return password.length >= 6;
-  }
-
+export class PasswordValidatorTwo extends IPasswordValidator {
+    constructor() {
+        const rules = [
+            (password: string) => password.length >= 6,
+        ]
+        super(rules);
+    }
 }
