70c834aa5d2432a378c95c3e7de66c4278d66525
Henry Nomichith <hnomichith@kpler.com>
2024-02-28 15:16:51 +0100

    henry turn

--- typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
@@ -3,3 +3,13 @@ describe("Example test", () => {
         expect(1 + 1).toBe(2);
     });
 });
+
+const validate_password = (password: string): boolean => {
+    return true;
+}
+
+describe("The password validator", () => {
+    if("should not have less than characters", () => {
+        const result = validate_password("7charas");
+    });
+});
