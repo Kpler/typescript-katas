c85316a2986beb01bfce5c4167f9ba08f35361f5
Simon Boeuf <sboeuf@ekinox.io>
2024-02-28 16:08:34 +0100

    yet another test

--- typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
@@ -1,11 +1,12 @@
-import {isPasswordValid} from "./passwordValidation";
+import isPasswordValid from "./passwordValidation";
 
 describe("The password validator", () => {
     it.each([
         ['should not have less than 8 characters', '7charas', false],
-        ['should contain capital letter', 'characte', false],
+        ['should contain capital letter', '0characte_', false],
         ['should be valid with complex password', '8Charats_', true],
-        ['should contain a lower case letter', 'CHARACTER', false]
+        ['should contain a lower case letter', 'CHARACTER', false],
+        ['should contain a number', 'CCCharats_', false],
     ])('%s', (_, password, expectedResult) =>{
         const result = isPasswordValid(password);
         expect(result).toBe(expectedResult)
--- typescript-backend-katas/src/passwordValidation/passwordValidation.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.ts
@@ -1,4 +1,22 @@
+export default (password: string): boolean => {
+    return isPasswordLongEnough(password)
+        && containsAnUppercase(password)
+        && containsALowercase(password)
+        && containsANumber(password);
+}
 
-export const isPasswordValid = (password: string): boolean => {
-    return password.length >= 8 && /[A-Z]/.test(password) && /[a-z]/.test(password);
+function containsANumber(password: string) {
+    return /\d/.test(password);
+}
+
+function isPasswordLongEnough(password: string) {
+    return password.length >= 8;
+}
+
+function containsAnUppercase(password: string) {
+    return /[A-Z]/.test(password);
+}
+
+function containsALowercase(password: string) {
+    return /[a-z]/.test(password);
 }
