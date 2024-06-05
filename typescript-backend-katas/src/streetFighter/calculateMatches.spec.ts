3e535ca6826cf0ade850df6e495b4ef7bb5f2ac7
manolisan <manastasiou@kpler.com>
2024-06-05 16:55:32 +0300

    feat: first test

---
+++ typescript-backend-katas/src/streetFighter/calculateMatches.spec.ts
@@ -0,0 +1,11 @@
+describe("calculateMatches", () => {
+    it('given a list of fighters and list of matches, should return the fighters ranking', () => {
+
+        const fighters = ['zangief', 'blanka']
+        const matches = [{timeslot: 1, home: 'zangief', roundsWon1: 1, roundsWon2: 2, away: 'blanka'}]
+
+        const ranking = calculateMatches(fighters, matches)
+
+        expect(ranking).toEqual(['blanka', 'zangief']);
+    })
+});
