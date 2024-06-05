de5b67c43ce8d3be362933381b4fea04e1b4211d
pmaroul <pmaroulidis@kpler.com>
2024-06-05 17:13:56 +0300

    initial commit

---
+++ typescript-backend-katas/src/streetFighter/street-fighter.spec.ts
@@ -0,0 +1,26 @@
+describe("List of fighters", () => {
+    it("should parse the CSV and return the correct ranking", async () => {
+        //Given
+        const listOfFighters = ["ryuu"]
+        //When
+        const listOfMatches = [
+            {
+                timeslot: 1,
+                home: "ryuu",
+                roundsWon1: 1,
+                roundsWon2: 2,
+                away: "e honda"
+            },
+            {
+                timeslot: 2,
+                home: "blanka",
+                roundsWon1: 1,
+                roundsWon2: 2,
+                away: "major guile"
+            }
+        ]
+        //Then
+    })
+})
+
+
