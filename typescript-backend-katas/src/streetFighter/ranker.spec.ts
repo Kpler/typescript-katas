bac733ef31a3286b47d67b88f2ce73ff53abc4e5
Henry Nomichith <hnomichith@kpler.com>
2024-06-21 11:24:20 +0200

    henry turn

--- typescript-backend-katas/src/streetFighter/ranker.spec.ts
+++ typescript-backend-katas/src/streetFighter/ranker.spec.ts
@@ -8,13 +8,17 @@ describe("computeRanking", () => {
 
   it('should return first the winner, then the loser when there is one match', () => {
     // Given
-    const matches = [new Match("chun-li", "sagat", 2, 1)];
+    const matches = [
+      new Match("chun-li", "sagat", 2, 1)
+    ];
 
     // When
     const ranking = computeRanking(matches)
 
     // Then
-    expect(ranking[0]).toEqual("chun-li")
-    expect(ranking[1]).toEqual("sagat")
+    expect(ranking).toStrictEqual([
+      {name: "chun-li", points: 3, rank: 1},
+      {name: "sagat", points: 0, rank: 2}
+    ]);
   })
 })
--- typescript-backend-katas/src/streetFighter/ranker.ts
+++ typescript-backend-katas/src/streetFighter/ranker.ts
@@ -13,6 +13,52 @@ export class Match {
     }
 }
 
+enum MatchResult {
+    HOME_WIN,
+    AWAY_WIN,
+    DRAW,
+}
+
+interface SimplifiedMatch {
+    home: string;
+    away: string;
+    result: MatchResult;
+}
+
+function computeMatchResult(match: Match): SimplifiedMatch {
+    if (match.homeRounds > match.awayRounds) {
+        return { home: match.homeFighter, away: match.awayFighter, result: MatchResult.HOME_WIN };
+    } else if (match.homeRounds < match.awayRounds) {
+        return { home: match.homeFighter, away: match.awayFighter, result: MatchResult.AWAY_WIN };
+    } else {
+        return { home: match.homeFighter, away: match.awayFighter, result: MatchResult.DRAW };
+    }
+}
+
+
+function incrementFightersScore(pointsPerFighter: Map<string, number>, match: SimplifiedMatch): void {
+    if (match.result === MatchResult.HOME_WIN) {
+        pointsPerFighter.set(match.home, (pointsPerFighter.get(match.home) || 0) + 3);
+        pointsPerFighter.set(match.away, (pointsPerFighter.get(match.away) || 0) + 0);
+    } else if (match.result === MatchResult.AWAY_WIN) {
+        pointsPerFighter.set(match.home, (pointsPerFighter.get(match.home) || 0) + 0);
+        pointsPerFighter.set(match.away, (pointsPerFighter.get(match.away) || 0) + 3);
+    } else {
+        pointsPerFighter.set(match.home, (pointsPerFighter.get(match.home) || 0) + 1);
+        pointsPerFighter.set(match.away, (pointsPerFighter.get(match.away) || 0) + 1);
+    }
+}
+
 export function computeRanking(matches: Match[]): string[] {
-    return [];
+    const pointsPerFighter: Map<string, number> = new Map();
+
+    const simplifiedMatches: SimplifiedMatch[] = matches.map(computeMatchResult);
+
+    simplifiedMatches.forEach(match => incrementFightersScore(pointsPerFighter, match));
+
+    const ranking = Array.from(pointsPerFighter.entries())
+        .sort((a, b) => b[1] - a[1])
+        .map(entry => entry[0]);
+
+    return ranking;
 }
