aa15259be79d6546d7235c8926b555a9c9982b74
Jan Warnecke-Hasselberg <jhasselberg@kpler.com>
2024-06-21 10:40:39 +0200

    ✨ (skeleton): adding signature for calculate matches

---
+++ typescript-backend-katas/src/streetFighter/calculateMatches.ts
@@ -0,0 +1,11 @@
+type match = {
+    timeslot: number,
+    home: string,
+    roundsWon1: number,
+    roundsWon2: number,
+    away: string,
+}
+
+export function calculateMatches(matches: string[]): string[] {
+    return []
+}
