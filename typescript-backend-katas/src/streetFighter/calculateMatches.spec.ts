ff98793d4952bcaaa9c3ac1a54b57ade24ef5316
Jan Warnecke-Hasselberg <jhasselberg@kpler.com>
2024-06-21 11:36:50 +0200

    ✨ (features): latest state

--- typescript-backend-katas/src/streetFighter/calculateMatches.spec.ts
+++ typescript-backend-katas/src/streetFighter/calculateMatches.spec.ts
@@ -13,6 +13,18 @@ describe("calculateMatches", () => {
             {timeslot: 1, home: 'Jan', roundsWon1: 2, roundsWon2: 1, away: 'Manolis'}
         ]
         const ranking = calculateMatches(matches);
-        expect(ranking).toStrictEqual(['Jan', 'Manolis']);
+        expect(ranking).toStrictEqual([{fighter: 'Jan', points: 3}, {fighter: 'Manolis', points: 0}]);
+    });
+
+    it("for 4 matches, return the winner in the first place in the ranking", () => {
+
+        const matches: Match[] = [
+            {timeslot: 1, home: 'Jan', roundsWon1: 1, roundsWon2: 1, away: 'Manolis'},
+            {timeslot: 2, home: 'Jan', roundsWon1: 2, roundsWon2: 1, away: 'Manolis'},
+            {timeslot: 3, home: 'Jan', roundsWon1: 1, roundsWon2: 2, away: 'Manolis'},
+            {timeslot: 4, home: 'Jan', roundsWon1: 1, roundsWon2: 3, away: 'Manolis'},
+        ]
+        const ranking = calculateMatches(matches);
+        expect(ranking).toStrictEqual([{fighter: 'Manolis', points: 7}, {fighter: 'Jan', points: 4}]);
     });
 });
--- typescript-backend-katas/src/streetFighter/calculateMatches.ts
+++ typescript-backend-katas/src/streetFighter/calculateMatches.ts
@@ -6,6 +6,49 @@ export type Match = {
     away: string,
 }
 
-export function calculateMatches(matches: Match[]): string[] {
-    return []
+export type RankingEntry = {
+    fighter: string,
+    points: number
+}
+
+export type Ranking = Record<string, number>
+
+export function sortRanking(ranking: Ranking): RankingEntry[] {
+    const sortedRanking = Object.entries(ranking).sort((a, b) => a[1] < b[1] ? 1 : -1);
+    return sortedRanking.map(e => ({fighter: e[0], points: e[1]}))
+}
+
+export function calculateMatches(matches: Match[]): RankingEntry[] {
+    if(matches.length === 0) {
+        return []
+    }
+
+    const ranking = matches.reduce(
+        (acc: Ranking, cur: Match) => {
+            acc[cur.home] = 0
+            acc[cur.away] = 0
+            return acc;
+        },
+        {}
+    )
+
+    matches.reduce(
+        (acc: Ranking, cur: Match) => {
+
+            if (cur.roundsWon1 > cur.roundsWon2) {
+                acc[cur.home] += 3
+            } else if (cur.roundsWon1 === cur.roundsWon2) {
+                acc[cur.home] += 1
+                acc[cur.away] += 1
+            } else {
+                acc[cur.away] += 3
+            }
+
+            return acc
+        },
+        ranking
+    )
+
+
+    return sortRanking(ranking)
 }
