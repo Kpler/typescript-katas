187e44a3fc4281ee74cf66e4782b9c6836c5fd25
Daniel Merkel <dmerkel@kpler.com>
2024-06-21 11:31:58 +0200

    test: make test green

--- typescript-backend-katas/src/streetFighter/street-fight.spec.ts
+++ typescript-backend-katas/src/streetFighter/street-fight.spec.ts
@@ -18,7 +18,10 @@ function calculateRanking(matches: Match[]): Array<Ranking> {
     const arr: {
         [name: string]: number
     } = {};
-    matches.forEach((match: Match) =>{
+    matches.forEach((match: Match) => {
+        arr[match.home] = arr[match.home] || 0
+        arr[match.away] = arr[match.away] || 0
+
         arr[match.home] += match.roundsWon1 > match.roundsWon2 ? 3 : (match.roundsWon1 === match.roundsWon2 ? 1 : 0);
         arr[match.away] += match.roundsWon2 > match.roundsWon1 ? 3 : (match.roundsWon1 === match.roundsWon2 ? 1 : 0);
     });
@@ -32,40 +35,65 @@ function calculateRanking(matches: Match[]): Array<Ranking> {
         });
     }
 
-    return result;
+    return result.sort((a, b) => b.points - a.points).map((ranking, index) => {
+        return {
+            ...ranking,
+            rank: index + 1
+        }
+    });
 }
 
 describe("Street Fighter", () => {
     it("should return a ranking", async () => {
         // GIVEN
         const expectedRanking: Array<Ranking> = [{
-            fighterId: 1,
-            rank: 1,
-            points: 2
-        }];
+                fighterId: 'chun-li',
+                rank: 1,
+                points: 3
+            },
+            {
+                fighterId: 'blanka',
+                rank: 2,
+                points: 3
+            },
+            {
+                fighterId: 'dhalsim',
+                rank: 3,
+                points: 1
+            },
+            {
+                fighterId: 'zangief',
+                rank: 4,
+                points: 1
+            },
+            {
+                fighterId: 'sagat',
+                rank: 5,
+                points: 0
+            }];
 
         // WHEN
         //const matches: Match[] = await parseCsvFile('./sources/sfplDb.csv', ',');
         const matches: Match[] = [{
-            timeslot:1,
-            home: 'chun-li',
-            roundsWon1: 2,
-            roundsWon2: 1,
-            away: 'sagat',
-        },
-        {
-            timeslot:2,
-            home: 'dhalsim',
-            roundsWon1: 2,
-            roundsWon2: 2,
-            away: 'zangief',
-        },{
-            timeslot:3,
-            home: 'blanka',
-            roundsWon1: 0,
-            roundsWon2: 2,
-            away: 'sagat',
-        }];
+                timeslot: 1,
+                home: 'chun-li',
+                roundsWon1: 2,
+                roundsWon2: 1,
+                away: 'sagat',
+            },
+            {
+                timeslot: 2,
+                home: 'dhalsim',
+                roundsWon1: 2,
+                roundsWon2: 2,
+                away: 'zangief',
+            }, {
+                timeslot: 3,
+                home: 'blanka',
+                roundsWon1: 2,
+                roundsWon2: 0,
+                away: 'sagat',
+            }];
         const result = calculateRanking(matches);
 
         // THEN
