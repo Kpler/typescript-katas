97b5f20da7de582ff37843c8ffec908f5b03baad
florian.ludwig <fludwig@kpler.com>
2024-07-03 16:05:45 +0200

    add for loop

--- typescript-backend-katas/src/diamond/diamond.ts
+++ typescript-backend-katas/src/diamond/diamond.ts
@@ -1,14 +1,20 @@
 export function createDiamond(level: number = 1) {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-    if (level === 1) {
-        return "A";
-    }
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  if (level === 1) {
+    return "A";
+  }
 
-    for (let levelIndex = 0; levelIndex < level; levelIndex++) {
-        if (levelIndex === 0) {
-            return "A";
-        }
+  // for (let levelIndex = 0; levelIndex < level; levelIndex++) {
+  //     if (levelIndex === 0) {
+  //         return "A";
+  //     }
 
-        
+  if (level === 2) {
+    const firstLetter = characters[level - 1];
+    for (let i = 0; i < level; i++) {
+      if (i === 0) {
+        return `*${firstLetter}*`;
+      }
     }
+  }
 }
