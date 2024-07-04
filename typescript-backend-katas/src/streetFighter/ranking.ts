661e32a9bfbb55df45223f64e0817cd25576a5b4
Nicolas Guignard <nicolas065@gmail.com>
2024-07-04 16:02:35 +0200

    feat:  wip

--- typescript-backend-katas/src/streetFighter/ranking.ts
+++ typescript-backend-katas/src/streetFighter/ranking.ts
@@ -42,8 +42,22 @@ export function getRanking(matches: Match[]): RankingRow[] {
     const pointsPerFighter: Record<string, number> = {};
 
     matchesResult.forEach(matchResult => {
-        
+        if (matchResult.result === Result.HomeWin) {
+            pointsPerFighter[matchResult.match.homeFighter] =  (pointsPerFighter[matchResult.match.homeFighter] ?? 0) + 3
+            pointsPerFighter[matchResult.match.awayFighter] =  (pointsPerFighter[matchResult.match.awayFighter] ?? 0)
+        } else  if (matchResult.result === Result.AwayWin) {
+            pointsPerFighter[matchResult.match.awayFighter] =  (pointsPerFighter[matchResult.match.awayFighter] ?? 0) + 3
+            pointsPerFighter[matchResult.match.homeFighter] =  (pointsPerFighter[matchResult.match.homeFighter] ?? 0)
+        } else {
+            pointsPerFighter[matchResult.match.awayFighter] =  (pointsPerFighter[matchResult.match.awayFighter] ?? 0) + 1
+            pointsPerFighter[matchResult.match.homeFighter] =  (pointsPerFighter[matchResult.match.homeFighter] ?? 0) + 1
+        }
     });
+    Object.entries(pointsPerFighter)
+      .sort(([fighterA, aPoints], [fighterB, bPoints]) => aPoints - bPoints)
+      .map((row, index) => {
+
+    })
 
     if (matchResult.result === Result.HomeWin) {
         return [
