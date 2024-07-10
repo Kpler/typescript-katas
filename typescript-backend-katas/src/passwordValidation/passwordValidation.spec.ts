99b893a6b35bcb2170ad13d6bf813a8740f8de6e
Daniel Merkel <dmerkel@kpler.com>
2024-07-10 15:30:55 +0200

    feat: added lowercase

--- typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
@@ -3,7 +3,7 @@ import {isPasswordValid} from "./passwordValidation";
 describe("Password validation", () => {
     it("should check, that the password has more than 8 characters", () => {
         // GIVEN
-        const password = '123456789';
+        const password = 'aAfa3456789';
 
         // WHEN
         const result = isPasswordValid(password)
@@ -14,14 +14,25 @@ describe("Password validation", () => {
 
     it("should check, that the password has capital letters", () => {
         // GIVEN
-        const password = 'a23456789'
+        const password = 'aAfa3456789'
 
         // WHEN
         const result = isPasswordValid(password);
 
         // THEN
-        expect(result).toBeFalsy();
+        expect(result).toBe(true);
     })
 
+    it("should check, that the password has lowercase letters", () => {
+        // GIVEN
+        const password = 'aAfa3456789'
 
+        // WHEN
+        const result = isPasswordValid(password);
+
+        // THEN
+        expect(result).toBe(true);
+    })
+    
+    
 });
--- typescript-backend-katas/src/passwordValidation/passwordValidation.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.ts
@@ -1,3 +1,6 @@
-export function isPasswordValid(password: string) {
-    return password.length > 8 && password.match(`[A-Z].*`);
+export function isPasswordValid(password: string): boolean {
+    const upperCaseCheck = new RegExp('[A-Z].*');
+    const lowerCaseCheck = new RegExp('[a-z].*');
+
+    return password.length > 8 && upperCaseCheck.test(password) && lowerCaseCheck.test(password)
 }
