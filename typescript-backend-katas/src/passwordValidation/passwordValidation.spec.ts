cda695e176d6d2f100589dae27dbe8ce9a455281
Daniel Merkel <dmerkel@kpler.com>
2024-07-10 15:38:49 +0200

    feat: added underscore

--- typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.spec.ts
@@ -3,7 +3,7 @@ import {isPasswordValid} from "./passwordValidation";
 describe("Password validation", () => {
     it("should check, that the password has more than 8 characters", () => {
         // GIVEN
-        const password = 'aAfa3456789';
+        const password = '_aAfa3456789';
 
         // WHEN
         const result = isPasswordValid(password)
@@ -14,7 +14,7 @@ describe("Password validation", () => {
 
     it("should check, that the password has capital letters", () => {
         // GIVEN
-        const password = 'aAfa3456789'
+        const password = '_aAfa3456789'
 
         // WHEN
         const result = isPasswordValid(password);
@@ -25,7 +25,7 @@ describe("Password validation", () => {
 
     it("should check, that the password has lowercase letters", () => {
         // GIVEN
-        const password = 'aAfa3456789'
+        const password = '_aAfa3456789'
 
         // WHEN
         const result = isPasswordValid(password);
@@ -33,6 +33,27 @@ describe("Password validation", () => {
         // THEN
         expect(result).toBe(true);
     })
-    
-    
+
+    it("should check, that the password has min. one number", () => {
+        // GIVEN
+        const password = '_aAfa3456789'
+
+        // WHEN
+        const result = isPasswordValid(password);
+
+        // THEN
+        expect(result).toBe(true);
+    })
+
+    it("should check, that the password has min. one underscore", () => {
+        // GIVEN
+        const password = '_aAfa3456789'
+
+        // WHEN
+        const result = isPasswordValid(password);
+
+        // THEN
+        expect(result).toBe(true);
+    })
+
 });
--- typescript-backend-katas/src/passwordValidation/passwordValidation.ts
+++ typescript-backend-katas/src/passwordValidation/passwordValidation.ts
@@ -1,6 +1,8 @@
 export function isPasswordValid(password: string): boolean {
     const upperCaseCheck = new RegExp('[A-Z].*');
     const lowerCaseCheck = new RegExp('[a-z].*');
+    const numberCheck = new RegExp('[0-9].*')
+    const underscoreCheck = new RegExp('_.*')
 
-    return password.length > 8 && upperCaseCheck.test(password) && lowerCaseCheck.test(password)
+    return password.length > 8 && upperCaseCheck.test(password) && lowerCaseCheck.test(password) && numberCheck.test(password) && underscoreCheck.test(password)
 }
