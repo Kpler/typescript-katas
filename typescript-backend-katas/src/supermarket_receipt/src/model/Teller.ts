dcf2809856a6760b5642f694925d1680693961a4
ejaoua <ejaoua@kpler.com>
2024-08-28 17:20:15 +0200

    fix(Teller): remove if

--- typescript-backend-katas/src/supermarket_receipt/src/model/Teller.ts
+++ typescript-backend-katas/src/supermarket_receipt/src/model/Teller.ts
@@ -44,13 +44,12 @@ export class Teller {
         }
 
         // TODO: Add a check which bundles are appliable.
-        if (this.bundles.length > 0) {
-            this.bundles.map((bundle) => {
-                receipt.addDiscountBundleInCatalog(
-                    this.calculateBundleDiscountAmount(bundle));
-            })
-            theCart.handleOffers(receipt, this.offers, this.catalog);
-        }
+        this.bundles.map((bundle) => {
+            receipt.addDiscountBundleInCatalog(
+                this.calculateBundleDiscountAmount(bundle));
+        })
+        theCart.handleOffers(receipt, this.offers, this.catalog);
+
 
         return receipt;
     }
