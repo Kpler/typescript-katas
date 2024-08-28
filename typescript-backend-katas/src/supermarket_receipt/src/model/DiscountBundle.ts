16f6dd8523c3902da3fe38d6f6b1ff4b57429e1d
rekahrv <rhorvath@kpler.com>
2024-08-28 16:27:50 +0200

    add class DiscountBundle

---
+++ typescript-backend-katas/src/supermarket_receipt/src/model/DiscountBundle.ts
@@ -0,0 +1,10 @@
+import { Product } from "./Product"
+
+export class DiscountBundle {
+
+    constructor(public readonly products: [Product],
+        public readonly description: string,
+        public readonly rate: number
+    ) {
+    }
+}
