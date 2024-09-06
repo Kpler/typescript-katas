b23e5d4ea2f9de17e69dd39efb1a73ccb1c955eb
jromaidis <jordan.romaidis@marinetraffic.com>
2024-09-06 16:52:17 +0300

    Gather all exceptions at one file

---
+++ typescript-backend-katas/src/tellDontAsk/utils/exceptions.ts
@@ -0,0 +1,11 @@
+export class ApprovedOrderCannotBeRejectedException extends Error {}
+
+export  class OrderCannotBeShippedException extends Error {}
+
+export  class OrderCannotBeShippedTwiceException extends Error {}
+
+export  class RejectedOrderCannotBeApprovedException extends Error {}
+
+export  class ShippedOrdersCannotBeChangedException extends Error {}
+
+export  class UnknownProductException extends Error {}
