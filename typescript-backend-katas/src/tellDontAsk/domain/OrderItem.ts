36f8913dfaca121efb2e0f7643a21f155f1d7bd1
DavidPalmi <92101787+DavidPalmi@users.noreply.github.com>
2024-09-06 10:39:57 -0400

    rrefactor orderitem constructor

--- typescript-backend-katas/src/tellDontAsk/domain/OrderItem.ts
+++ typescript-backend-katas/src/tellDontAsk/domain/OrderItem.ts
@@ -14,8 +14,8 @@ class OrderItem {
     const unitaryTax: number = Math.round(product.getPrice() / 100 * product.getCategory().getTaxPercentage() * 100) / 100;
     const unitaryTaxedAmount: number = Math.round((product.getPrice() + unitaryTax) * 100) / 100;
 
-    this.tax = unitaryTax * itemRequest.getQuantity();
-    this.taxedAmount = Math.round(unitaryTaxedAmount * itemRequest.getQuantity() * 100) / 100;
+    this.tax = unitaryTax * this.quantity;
+    this.taxedAmount = Math.round(unitaryTaxedAmount * this.quantity * 100) / 100;
 
   }
 
