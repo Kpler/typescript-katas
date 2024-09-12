0b2ff4bc6054b51bf3c9bda237210edcdef0b4e8
manolisan <manastasiou@kpler.com>
2024-09-12 12:15:06 +0300

    chore: move print receipt item to domain class

--- typescript-backend-katas/src/supermarket_receipt/src/ReceiptPrinter.ts
+++ typescript-backend-katas/src/supermarket_receipt/src/ReceiptPrinter.ts
@@ -1,7 +1,4 @@
-import {ProductUnit} from "./model/ProductUnit"
-import {ReceiptItem} from "./model/ReceiptItem"
 import {Receipt} from "./model/Receipt"
-import {Discount} from "./model/Discount";
 
 export class ReceiptPrinter {
 
@@ -11,7 +8,7 @@ export class ReceiptPrinter {
     public printReceipt( receipt: Receipt): string {
         let result = "";
         for (const item of receipt.getItems()) {
-            result += this.printReceiptItem(item);
+            result += item.print(this.columns);
         }
         for (const discount of receipt.getDiscounts()) {
             result += discount.print(this.columns)
@@ -34,25 +31,8 @@ export class ReceiptPrinter {
         }).format(number)
     }
 
-    private static presentQuantity( item: ReceiptItem): string  {
-        return ProductUnit.Each == item.product.unit
-            // TODO make sure this is the simplest way to make something similar to the java version
-                ? new Intl.NumberFormat('en-UK', {maximumFractionDigits: 0}).format(item.quantity)
-                : new Intl.NumberFormat('en-UK', {minimumFractionDigits: 3}).format(item.quantity);
-    }
 
     public static getWhitespace(whitespaceSize: number): string {
         return " ".repeat(whitespaceSize);
-    }
-
-    private printReceiptItem(receiptItem: ReceiptItem): string {
-        const price = ReceiptPrinter.format2Decimals(receiptItem.totalPrice);
-        const quantity = ReceiptPrinter.presentQuantity(receiptItem);
-        const name = receiptItem.product.name;
-        const unitPrice = ReceiptPrinter.format2Decimals(receiptItem.price);
-        const whitespaceSize = this.columns - name.length - price.length;
-
-        const moreThan1ItemText = `  ${unitPrice} * ${quantity}\n`;
-        return `${name}${ReceiptPrinter.getWhitespace(whitespaceSize)}${price}\n${receiptItem.quantity != 1 ? moreThan1ItemText : ""}`
     }
 }
--- typescript-backend-katas/src/supermarket_receipt/src/model/ReceiptItem.ts
+++ typescript-backend-katas/src/supermarket_receipt/src/model/ReceiptItem.ts
@@ -1,4 +1,6 @@
 import {Product} from "./Product"
+import {ReceiptPrinter} from "../ReceiptPrinter";
+import {ProductUnit} from "./ProductUnit";
 
 export class ReceiptItem {
 
@@ -6,5 +8,23 @@ export class ReceiptItem {
                        public readonly quantity: number,
                        public readonly price: number,
                        public totalPrice: number) {
+    }
+
+    public print(columns: number): string {
+        const price = ReceiptPrinter.format2Decimals(this.totalPrice);
+        const quantity = this.presentQuantity()
+        const name = this.product.name;
+        const unitPrice = ReceiptPrinter.format2Decimals(this.price);
+        const whitespaceSize = columns - name.length - price.length;
+
+        const moreThan1ItemText = `  ${unitPrice} * ${quantity}\n`;
+        return `${name}${ReceiptPrinter.getWhitespace(whitespaceSize)}${price}\n${this.quantity != 1 ? moreThan1ItemText : ""}`
+    }
+
+    private presentQuantity(): string  {
+        return ProductUnit.Each == this.product.unit
+            // TODO make sure this is the simplest way to make something similar to the java version
+            ? new Intl.NumberFormat('en-UK', {maximumFractionDigits: 0}).format(this.quantity)
+            : new Intl.NumberFormat('en-UK', {minimumFractionDigits: 3}).format(this.quantity);
     }
 }
