4f1001cee0f10c5a52c7c8f0fbe15d6de684c457
Simon Boeuf <sboeuf@ekinox.io>
2024-10-04 15:27:06 +0200

    second tests

--- typescript-backend-katas/src/streetFighter/rankingMatches.spec.ts
+++ typescript-backend-katas/src/streetFighter/rankingMatches.spec.ts
@@ -1,15 +1,23 @@
 import {computeRanking} from "./rankingMatches";
 
 describe("When we receive an empty list of matches", () => {
-  it("should return an empty array", () => {
-    const result = computeRanking([]);
-    expect(result).toEqual([]);
-  });
+    it("should return an empty array", () => {
+        const result = computeRanking([]);
+        expect(result).toEqual([]);
+    });
 });
 
 describe("When a list with one match with a winner", () => {
-  it("should return the ", () => {
-    const result = computeRanking([]);
-    expect(result).toEqual([]);
-  });
+    it("should return the ranking with the correct number of points for the winner and the loser", () => {
+        const matches = [new Match("David", 2, "Nico", 1)]
+
+        const result = computeRanking(matches);
+
+        expect(result).toEqual(
+            [
+                new RankedFighter("David", 3),
+                new RankedFighter("Nico", 0)
+            ]
+        );
+    });
 });
