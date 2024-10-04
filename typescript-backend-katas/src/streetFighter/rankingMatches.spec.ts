9b8b33da6817661d2996074bddad611e778273c4
Nicolas Guignard <nicolas065@gmail.com>
2024-10-04 15:43:41 +0200

    feat: test away fighter wins

--- typescript-backend-katas/src/streetFighter/rankingMatches.spec.ts
+++ typescript-backend-katas/src/streetFighter/rankingMatches.spec.ts
@@ -9,7 +9,7 @@ describe("When we receive an empty list of matches", () => {
     });
 });
 
-describe("When a list with one match with a winner", () => {
+describe("When a list with one match with the home fighter who wins", () => {
     it("should return the ranking with the correct number of points for the winner and the loser", () => {
         const matches = [new Match("David", 2, "Nico", 1)]
 
@@ -20,6 +20,21 @@ describe("When a list with one match with a winner", () => {
                 new RankedFighter("David", 3),
                 new RankedFighter("Nico", 0)
             ]
+        );
+    });
+});
+
+describe("When a list with one match with the away fighter who wins", () => {
+    it("should return the ranking with the correct number of points for the winner and the loser", () => {
+        const matches = [new Match("David", 1, "Nico", 2 )]
+
+        const result = computeRanking(matches);
+
+        expect(result).toEqual(
+          [
+              new RankedFighter("Nico", 3),
+              new RankedFighter("David", 0)
+          ]
         );
     });
 });
--- typescript-backend-katas/src/streetFighter/rankingMatches.ts
+++ typescript-backend-katas/src/streetFighter/rankingMatches.ts
@@ -1,6 +1,18 @@
 import { Match } from "./match"
 import { RankedFighter } from "./rankedFighter"
 
-export const computeRanking = (matches: Match[]): Array<RankedFighter> => {
-  return [new RankedFighter()]
+const WINNER_SCORE = 3
+const LOSER_SCORE = 0
+const TIE_SCORE = 1
+
+export const computeRanking = (matches: Match[]): RankedFighter[] => {
+  if (matches.length === 0) {
+    return []
+  }
+
+  const rankedFighters = [
+    new RankedFighter(matches[0].fighterHome, WINNER_SCORE),
+    new RankedFighter(matches[0].fighterAway, LOSER_SCORE)
+  ]
+  return rankedFighters
 }
