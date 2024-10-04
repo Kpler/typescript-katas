6109a836f48f789fcb694932600f281f0401fa8a
Simon Boeuf <sboeuf@ekinox.io>
2024-10-04 16:21:38 +0200

    parameterize tests

--- typescript-backend-katas/src/streetFighter/rankingMatches.spec.ts
+++ typescript-backend-katas/src/streetFighter/rankingMatches.spec.ts
@@ -2,70 +2,58 @@ import { Match } from "./match";
 import { RankedFighter } from "./rankedFighter";
 import {computeRanking} from "./rankingMatches";
 
-describe("When we receive an empty list of matches", () => {
-    it("should return an empty array", () => {
-        const result = computeRanking([]);
-        expect(result).toEqual([]);
-    });
-});
-
-describe("When a list with one match with the home fighter who wins", () => {
-    it("should return the ranking with the correct number of points for the winner and the loser", () => {
-        const matches = [new Match("David", 2, "Nico", 1)]
-
-        const result = computeRanking(matches);
-
-        expect(result).toEqual(
-            [
+describe("computeRanking", () => {
+    it.each([
+        {
+            testTitle:"With an empty list of matches",
+            matches: [],
+            expectedRanking: []
+        },
+        {
+            testTitle:"With a list with one match with the home fighter who wins",
+            matches: [new Match("David", 2, "Nico", 1)],
+            expectedRanking: [
                 new RankedFighter("David", 3),
                 new RankedFighter("Nico", 0)
             ]
-        );
-    });
-});
-
-describe("When a list with one match with the away fighter who wins", () => {
-    it("should return the ranking with the correct number of points for the winner and the loser", () => {
-        const matches = [new Match("David", 1, "Nico", 2 )]
-
-        const result = computeRanking(matches);
-
-        expect(result).toEqual(
-          [
-              new RankedFighter("Nico", 3),
-              new RankedFighter("David", 0)
-          ]
-        );
-    });
-});
-
-describe("When a list with one match with a tie", () => {
-    it("should return the ranking with the correct number of points for both fighters", () => {
-        const matches = [new Match("David", 2, "Nico", 2 )]
-
-        const result = computeRanking(matches);
-
-        expect(result).toEqual(
-            [
+        },
+        {
+            testTitle:"With a list with one match with the away fighter who wins",
+            matches: [new Match("David", 1, "Nico", 2 )],
+            expectedRanking: [
+                new RankedFighter("Nico", 3),
+                new RankedFighter("David", 0)
+            ]
+        },
+        {
+            testTitle:"With a list with one match with a tie",
+            matches: [new Match("David", 2, "Nico", 2 )],
+            expectedRanking: [
                 new RankedFighter("David", 1),
                 new RankedFighter("Nico", 1)
             ]
-        );
-    });
-});
-
-describe("When a list with two match with 3 participants", () => {
-    it("should return the ranking with the correct number of points for every fighters", () => {
-        const matches = [new Match("David", 2, "Nico", 1 ), new Match("Simon", 2, "Nico", 1 ) ]
-
-        const result = computeRanking(matches);
-
-        expect(result).toEqual(
-            [
+        },
+        {
+            testTitle:"With a list with one match with a tie",
+            matches: [new Match("David", 2, "Nico", 2 )],
+            expectedRanking: [
+                new RankedFighter("David", 1),
+                new RankedFighter("Nico", 1)
+            ]
+        },
+        {
+            testTitle:"With a list with two match with 3 participants",
+            matches: [new Match("David", 2, "Nico", 1 ), new Match("Simon", 2, "Nico", 1 ) ],
+            expectedRanking: [
                 new RankedFighter("David", 3),
                 new RankedFighter("Simon", 3),
                 new RankedFighter("Nico", 0)
             ]
-        );
-    });
-});
+        },
+    ])("$testTitle", ({matches, expectedRanking}) => {
+        const result = computeRanking(matches);
+
+        expect(result).toEqual(expectedRanking);
+    })
+})
+
