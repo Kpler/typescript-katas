0ee3f1cea53115184f6c4d7381131c526178c808
Simon Boeuf <sboeuf@ekinox.io>
2024-10-04 16:47:33 +0200

    start to add the datasource

--- typescript-backend-katas/src/streetFighter/rankingMatches.ts
+++ typescript-backend-katas/src/streetFighter/rankingMatches.ts
@@ -1,12 +1,18 @@
 import { Fighter } from "./fighter"
 import {Match} from "./match"
 import {RankedFighter} from "./rankedFighter"
+import {parseJsonFile} from "./helpers/helpers";
 
 const WINNER_SCORE = 3
 const LOSER_SCORE = 0
 const TIE_SCORE = 1
 
-export const computeRanking = (match: Match[]): RankedFighter[] => {
+export const computeRankingFromDatasource = (fightersJSONDataSource: string, matches: []) => {
+  const fighters = parseJsonFile<Fighter>(fightersJSONDataSource);
+  return computeRanking(matches, fighters);
+}
+
+export const computeRanking = (match: Match[], fighters: Fighter[]): RankedFighter[] => {
   if (match.length === 0) {
     return []
   }
@@ -14,22 +20,26 @@ export const computeRanking = (match: Match[]): RankedFighter[] => {
   const rankedFighters: Record<string, RankedFighter> = {}
   match.forEach(match => {
     if (match.scoreHome > match.scoreAway) {
-      rankedFighters[match.fighterHome] = new RankedFighter(getFighterFromName(match.fighterHome), (rankedFighters[match.fighterHome]?.score ?? 0) + WINNER_SCORE)
-      rankedFighters[match.fighterAway] = new RankedFighter(getFighterFromName(match.fighterAway), (rankedFighters[match.fighterAway]?.score ?? 0) + LOSER_SCORE)
+      rankedFighters[match.fighterHome] = new RankedFighter(getFighterFromName(match.fighterHome, fighters), (rankedFighters[match.fighterHome]?.score ?? 0) + WINNER_SCORE)
+      rankedFighters[match.fighterAway] = new RankedFighter(getFighterFromName(match.fighterAway, fighters), (rankedFighters[match.fighterAway]?.score ?? 0) + LOSER_SCORE)
     }
     if (match.scoreHome < match.scoreAway) {
-      rankedFighters[match.fighterAway] = new RankedFighter(getFighterFromName(match.fighterAway), (rankedFighters[match.fighterAway]?.score ?? 0) + WINNER_SCORE)
-      rankedFighters[match.fighterHome] = new RankedFighter(getFighterFromName(match.fighterHome), (rankedFighters[match.fighterHome]?.score ?? 0) + LOSER_SCORE)
+      rankedFighters[match.fighterAway] = new RankedFighter(getFighterFromName(match.fighterAway, fighters), (rankedFighters[match.fighterAway]?.score ?? 0) + WINNER_SCORE)
+      rankedFighters[match.fighterHome] = new RankedFighter(getFighterFromName(match.fighterHome, fighters), (rankedFighters[match.fighterHome]?.score ?? 0) + LOSER_SCORE)
     }
     if (match.scoreHome === match.scoreAway) {
-      rankedFighters[match.fighterHome] = new RankedFighter(getFighterFromName(match.fighterHome), (rankedFighters[match.fighterHome]?.score ?? 0) + TIE_SCORE)
-      rankedFighters[match.fighterAway] = new RankedFighter(getFighterFromName(match.fighterAway), (rankedFighters[match.fighterAway]?.score ?? 0) + TIE_SCORE)
+      rankedFighters[match.fighterHome] = new RankedFighter(getFighterFromName(match.fighterHome, fighters), (rankedFighters[match.fighterHome]?.score ?? 0) + TIE_SCORE)
+      rankedFighters[match.fighterAway] = new RankedFighter(getFighterFromName(match.fighterAway, fighters), (rankedFighters[match.fighterAway]?.score ?? 0) + TIE_SCORE)
     }
   })
 
   return Object.values(rankedFighters).sort((a, b) => b.score - a.score)
 }
 
-const getFighterFromName = (name:string) => {
-  return new Fighter(0, name, undefined, undefined)
+const getFighterFromName = (name:string, fighters: Fighter[]) => {
+  const fighter = fighters.find(f => f.firstName === name);
+  if(!fighter) {
+    throw new Error("Fighter not found: " + name);
+  }
+  return fighter;
 }
