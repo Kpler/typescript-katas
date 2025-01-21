b76a87a571d489bbb1c16516d0783d334ea9b10e
Nabil Krause <nkrause@kpler.com>
2025-01-21 13:26:47 +0100

    feat: extend tests

--- typescript-backend-katas/src/robot/robot.spec.ts
+++ typescript-backend-katas/src/robot/robot.spec.ts
@@ -31,4 +31,28 @@ describe("Robot", () => {
         const expectedResult = {position: [0, 0], direction: "East"}
         expect(position).toEqual(expectedResult);
     })
+
+    it("should be able to make a step", () => {
+        //GIVEN
+        const args = "M";
+
+        // WHEN
+        const position = navigateRobot(args);
+
+        // THEN
+        const expectedResult = {position: [0, 1], direction: "North"};
+        expect(position).toEqual(expectedResult)
+    });
+
+    it("should be able to turn and then move", () => {
+        //GIVEN
+        const args = "LM";
+
+        // WHEN
+        const position = navigateRobot(args);
+
+        // THEN
+        const expectedResult = {position: [-1, 0], direction: "West"};
+        expect(position).toEqual(expectedResult)
+    })
 });
--- typescript-backend-katas/src/robot/robot.ts
+++ typescript-backend-katas/src/robot/robot.ts
@@ -1,15 +1,22 @@
 const enum Actions {
-    LEFT = 'L',
-    RIGHT = 'R'
+    Left = 'L',
+    Right = 'R',
+    Move = 'M'
 }
 
 export function navigateRobot(actions?: string) {
     let direction = 'North'
+    let position = [0, 0];
 
-    if (actions === Actions.LEFT) {
-        direction = 'West';
-    } else if (actions === Actions.RIGHT) {
-        direction = 'East';
+    for (const action of actions || []) {
+        if (actions === Actions.Left) {
+            direction = 'West';
+        } else if (actions === Actions.Right) {
+            direction = 'East';
+        } else if (actions === Actions.Move) {
+            position = [0, 1];
+            return {position, direction};
+        }
     }
 
     return {position: [0, 0], direction};
