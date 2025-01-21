431651d69a6d1306c994f552256b0b6ec8670b20
Daniel Merkel <dmerkel@kpler.com>
2025-01-21 13:17:58 +0100

    feat: actions

--- typescript-backend-katas/src/robot/robot.ts
+++ typescript-backend-katas/src/robot/robot.ts
@@ -1,4 +1,16 @@
+const enum Actions {
+    LEFT = 'L',
+    RIGHT = 'R'
+}
+
 export function navigateRobot(actions?: string) {
     let direction = 'North'
-    return {position: [0, 0], direction: actions ? "West" : "North"}
+
+    if (actions === Actions.LEFT) {
+        direction = 'West';
+    } else if (actions === Actions.RIGHT) {
+        direction = 'East';
+    }
+
+    return {position: [0, 0], direction};
 }
