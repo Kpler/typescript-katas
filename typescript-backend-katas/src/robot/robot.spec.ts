5c077c59b5f439106bed981bd2b352d790a5b9ed
Daniel Merkel <dmerkel@kpler.com>
2025-01-21 12:55:15 +0100

    test: added test for robot

--- typescript-backend-katas/src/robot/robot.spec.ts
+++ typescript-backend-katas/src/robot/robot.spec.ts
@@ -1,5 +1,20 @@
+function navigateRobot() {
+    return {}
+}
+
 describe("Example test", () => {
     it("should sum numbers correctly", () => {
         expect(1 + 1).toBe(2);
     });
+
+    it("should have init position", () => {
+        // GIVEN
+
+        // WHEN
+        const position = navigateRobot();
+
+        // THEN
+        const expectedResult = { position: [0, 0], direction: "North" };
+        expect(position).toEqual(expectedResult);
+    })
 });
