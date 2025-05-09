d18b68c6502eecb506db36795b9d29294b3c1ffc
Henry Nomichith <hnomichith@kpler.com>
2025-05-09 16:51:56 +0200

    second commit

--- typescript-backend-katas/src/pokerHand/game.spec.ts
+++ typescript-backend-katas/src/pokerHand/game.spec.ts
@@ -1,19 +1,118 @@
-import {computeRank} from "./game";
+import { computeRank, computeWinner, Suit, Rank, HandRank, ComputedRank } from "./game";
 
-describe("when computing a pair", () => {
-    it("should return the best matching hand rank with a pair being flagged", () => {
+const handWithOnePair = [
+    { rank: Rank.TWO, suit: Suit.SPADES },
+    { rank: Rank.ACE, suit: Suit.CLUBS },
+    { rank: Rank.JACK, suit: Suit.CLUBS },
+    { rank: Rank.NINE, suit: Suit.DIAMONDS },
+    { rank: Rank.EIGHT, suit: Suit.DIAMONDS },
+    { rank: Rank.TWO, suit: Suit.HEARTS },
+    { rank: Rank.FOUR, suit: Suit.HEARTS },
+];
+const handWithOnePairOfAces = [
+    { rank: Rank.TWO, suit: Suit.SPADES },
+    { rank: Rank.ACE, suit: Suit.CLUBS },
+    { rank: Rank.JACK, suit: Suit.CLUBS },
+    { rank: Rank.NINE, suit: Suit.DIAMONDS },
+    { rank: Rank.EIGHT, suit: Suit.DIAMONDS },
+    { rank: Rank.ACE, suit: Suit.HEARTS },
+    { rank: Rank.FOUR, suit: Suit.HEARTS },
+];
+const highCardHand = [
+    { rank: Rank.TWO, suit: Suit.SPADES },
+    { rank: Rank.ACE, suit: Suit.CLUBS },
+    { rank: Rank.JACK, suit: Suit.CLUBS },
+    { rank: Rank.NINE, suit: Suit.DIAMONDS },
+    { rank: Rank.EIGHT, suit: Suit.DIAMONDS },
+    { rank: Rank.THREE, suit: Suit.HEARTS },
+    { rank: Rank.FOUR, suit: Suit.HEARTS },
+];
+const handWithTwoPairs = [
+    { rank: Rank.TWO, suit: Suit.SPADES },
+    { rank: Rank.ACE, suit: Suit.CLUBS },
+    { rank: Rank.JACK, suit: Suit.CLUBS },
+    { rank: Rank.JACK, suit: Suit.DIAMONDS },
+    { rank: Rank.EIGHT, suit: Suit.DIAMONDS },
+    { rank: Rank.TWO, suit: Suit.HEARTS },
+    { rank: Rank.FOUR, suit: Suit.HEARTS },
+];
+const handWithThreeOfAKind = [
+    { rank: Rank.TWO, suit: Suit.SPADES },
+    { rank: Rank.ACE, suit: Suit.CLUBS },
+    { rank: Rank.TWO, suit: Suit.CLUBS },
+    { rank: Rank.JACK, suit: Suit.DIAMONDS },
+    { rank: Rank.EIGHT, suit: Suit.DIAMONDS },
+    { rank: Rank.TWO, suit: Suit.HEARTS },
+    { rank: Rank.FOUR, suit: Suit.HEARTS },
+];
+describe("computeRank", () => {
+    it("should return a pair when there is one among 7 cards", () => {
         // Given
         // When
+        const actual = computeRank(handWithOnePair);
+
+        // Then
+        expect(actual).toEqual({
+            handRank: HandRank.PAIR,
+            highestRankValue: Rank.TWO,
+        });
+    });
+
+    it("should return FOLDED if less than 7 cards are provided", () => {
+        // When
         const actual = computeRank([
-            {rank: Rank.TWO, suit: Suit.SPADES},
-            {rank: Rank.ACE, suit: Suit.CLUBS},
-            {rank: Rank.JACK, suit: Suit.CLUBS},
-            {rank: Rank.NINE, suit: Suit.DIAMONDS},
-            {rank: Rank.HEIGHT, suit: Suit.DIAMONDS},
-            {rank: Rank.TWO, suit: Suit.HEARTS},
-            {rank: Rank.FOUR, suit: Suit.HEARTS},
+            { rank: Rank.TWO, suit: Suit.SPADES },
+            { rank: Rank.ACE, suit: Suit.CLUBS },
         ])
         // Then
-        expect(actual).toBe(HandRank.PAIR);
+        expect(actual).toEqual({
+            handRank: HandRank.FOLDED,
+            highestRankValue: null,
+        });
+    });
+
+    it("should return a high card when there is nothing better among 7 cards", () => {
+        // When
+        const actual = computeRank(highCardHand);
+
+        // Then
+        expect(actual).toEqual({ handRank: HandRank.HIGH_CARD, highestRankValue: Rank.ACE, });
+    });
+
+    it("should return two pairs when there are two pairs among 7 cards", () => {
+        // When
+        const actual = computeRank(handWithTwoPairs);
+
+        // Then
+        expect(actual).toEqual({handRank: HandRank.TWO_PAIRS, highestRankValue: Rank.JACK});
+    });
+
+    it("should return 'three of a kind' when there is one among 7 cards", () => {
+        // When
+        const actual = computeRank(handWithThreeOfAKind);
+
+        // Then
+        expect(actual).toEqual({handRank: HandRank.THREE_OF_A_KIND, highestRankValue: Rank.TWO});
+    });
+});
+
+describe("computeWinner", () => {
+    it("should two pairs win against one pair", () => {
+        // Given
+        const playerOne = handWithOnePair;
+        const playerTwo = handWithTwoPairs;
+        // When
+        const actual = computeWinner(playerOne, playerTwo);
+        // Then
+        expect(actual).toEqual([{ winner: false }, { winner: true}])
+    });
+    it("should compute based on pair quality", () => {
+        // Given
+        const playerOne = handWithOnePairOfAces;
+        const playerTwo = handWithOnePair;
+        // When
+        const actual = computeWinner(playerOne, playerTwo);
+        // Then
+        expect(actual).toEqual([{ winner: true }, { winner: false}])
     });
 });
--- typescript-backend-katas/src/pokerHand/game.ts
+++ typescript-backend-katas/src/pokerHand/game.ts
@@ -1,24 +1,24 @@
 export enum Rank {
-    TWO = 2
-    THREE = 3
-    FOUR = 4
-    FIVE = 5
-    SIX = 6
-    SEVEN = 7
-    EIGHT = 8
-    NINE = 9
-    TEN = 10
-    JACK = 11
-    QUEEN = 12
-    KING = 13
-    ACE = 14
+    TWO = 2,
+    THREE = 3,
+    FOUR = 4,
+    FIVE = 5,
+    SIX = 6,
+    SEVEN = 7,
+    EIGHT = 8,
+    NINE = 9,
+    TEN = 10,
+    JACK = 11,
+    QUEEN = 12,
+    KING = 13,
+    ACE = 14,
 }
 
 export enum Suit {
-    SPADES = "spades"
-    CLUBS = "clubs"
-    DIAMONDS = "diamonds"
-    HEARTS = "hearts"
+    SPADES = "spades",
+    CLUBS = "clubs",
+    DIAMONDS = "diamonds",
+    HEARTS = "hearts",
 }
 
 export interface Card {
@@ -27,9 +27,79 @@ export interface Card {
 }
 
 export enum HandRank {
-
-    PAIR = 2
+    FOLDED = 0,
+    HIGH_CARD = 1,
+    PAIR = 2,
+    TWO_PAIRS = 3,
+    THREE_OF_A_KIND = 4,
+}
+export interface ComputedRank {
+    handRank: HandRank;
+    highestRankValue: Rank | null;
 }
 
-export const computeRank = (hand: Card[]): HandRank => {}
+export const computeRank = (hand: Card[]): ComputedRank => {
+    if (hand.length < 7) {
+        return {
+            handRank: HandRank.FOLDED,
+            highestRankValue: null,
+        };
+    }
 
+    const handRankCards = hand.reduce((acc, currentCard) => {
+        const currentRankArray = acc[currentCard.rank] ?? [];
+        currentRankArray.push(currentCard);
+        acc[currentCard.rank] = currentRankArray;
+        return acc;
+    }, {} as Record<Rank, Card[]>);
+
+    const threeKindsCards = Object.values(handRankCards).filter((cards) => cards.length === 3);
+    if (threeKindsCards.length === 1) {
+        return {
+            handRank: HandRank.THREE_OF_A_KIND,
+            highestRankValue: threeKindsCards[0][0].rank,
+        };
+    }
+
+    const pairs = Object.values(handRankCards).filter((cards) => cards.length === 2);
+    if (pairs.length === 1) {
+        return {
+            handRank: HandRank.PAIR,
+            highestRankValue: pairs[0][0].rank,
+        };
+    }
+    if (pairs.length === 2) {
+        return {
+            handRank: HandRank.TWO_PAIRS,
+            highestRankValue: pairs[pairs.length - 1][ pairs.length - 1].rank,
+        };
+    }
+
+    return {
+        handRank: HandRank.HIGH_CARD,
+        highestRankValue: Rank.TWO,
+    };
+}
+
+interface computeResult {
+    winner: boolean;
+}
+
+export const computeWinner = (hand1: Card[], hand2: Card[]): [computeResult, computeResult] => {
+    const rank1 = computeRank(hand1);
+    const rank2 = computeRank(hand2);
+
+    if (rank1 > rank2) {
+        return [{
+            winner: true,
+        }, {
+            winner: false,
+        }]
+    }
+
+    return [{
+        winner: false,
+    }, {
+        winner: true,
+    }]
+}
