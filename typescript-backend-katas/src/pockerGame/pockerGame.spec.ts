ab48c6faf0c0df65934fae4a257380d60127acaf
DavidPalmi <92101787+DavidPalmi@users.noreply.github.com>
2025-05-09 10:32:57 -0400

    david turn

--- typescript-backend-katas/src/pockerGame/pockerGame.spec.ts
+++ typescript-backend-katas/src/pockerGame/pockerGame.spec.ts
@@ -1,29 +1,42 @@
-import {PockerGame} from "./pockerGame";
+import { PockerGame } from "./pockerGame";
 
 describe("Texas Holdem", () => {
   it("should read cards of one player", () => {
-    const playerCards: string = 'Kc 9s Ks Kd 9d 3c 6d'
+    const playerCards: string = "Kc 9s Ks Kd 9d 3c 6d";
     const game = new PockerGame();
-    expect(game.readCards(playerCards)).toEqual(['Kc','9s','Ks','Kd','9d','3c','6d']);
+    expect(game.readCards(playerCards)).toEqual([
+      "Kc",
+      "9s",
+      "Ks",
+      "Kd",
+      "9d",
+      "3c",
+      "6d",
+    ]);
   });
 
   it.each([
-    { input: ['5c','9s','Ks','Qd','4d','3c','6d'],
-      result: {hasOnePair: false, hasTwoPair: false },
+    {
+      input: ["5c", "9s", "Ks", "Qd", "4d", "3c", "6d"],
+      result: { hasOnePair: false, hasTwoPair: false, isFlush: false },
     },
-      { input: ['Kc','9s','Ks','Qd','4d','3c','6d'],
-        result: {hasOnePair: true, hasTwoPair: false },
-      },{ input: ['Kc','Qs','Ks','Qd','4d','3c','6d'],
-        result: {hasOnePair: false, hasTwoPair: true },
-      },
-      { input: ['Kc','Qs','Ks','2d','Kd','3c','6d'],
-        result: {hasOnePair: false, hasTwoPair: false },
-      },
-      { input: ['Kc','Qs','Ks','Qd','7d','3c','3d'],
-        result: {hasOnePair: false, hasTwoPair: true },
-      }
-  ]
-  )("shouldEvaluateHand", ({input, result}) => {
+    {
+      input: ["Kc", "9s", "Ks", "Qd", "4d", "3c", "6d"],
+      result: { hasOnePair: true, hasTwoPair: false, isFlush: false },
+    },
+    {
+      input: ["Kc", "Qs", "Ks", "Qd", "4d", "3c", "6d"],
+      result: { hasOnePair: false, hasTwoPair: true, isFlush: false },
+    },
+    {
+      input: ["Kc", "Qs", "Ks", "Qd", "7d", "3c", "3d"],
+      result: { hasOnePair: false, hasTwoPair: true, isFlush: false },
+    },
+    {
+      input: ["Ks", "Qs", "7d", "2s", "3s", "3c", "6s"],
+      result: { hasOnePair: false, hasTwoPair: false, isFlush: true },
+    },
+  ])("shouldEvaluateHand", ({ input, result }) => {
     const game = new PockerGame();
     expect(game.evaluateHand(input)).toEqual(result);
   });
--- typescript-backend-katas/src/pockerGame/pockerGame.ts
+++ typescript-backend-katas/src/pockerGame/pockerGame.ts
@@ -1,39 +1,47 @@
-
 type handRanking = {
-    hasOnePair: boolean,
-    hasTwoPair: boolean,
-}
+  hasOnePair: boolean;
+  hasTwoPair: boolean;
+  isFlush: boolean;
+};
 export class PockerGame {
-    counts = new Map<string, number>();
-    handRank:handRanking = {
-        hasOnePair: false,
-        hasTwoPair: false,
-    }
-    readCards(a: string): string[] {
-        return a.split(' ');
-    }
+  faceValueCounts = new Map<string, number>();
+  suitValueCounts = new Map<string, number>();
+  handRank: handRanking = {
+    hasOnePair: false,
+    hasTwoPair: false,
+    isFlush: false,
+  };
+  readCards(a: string): string[] {
+    return a.split(" ");
+  }
 
-    evaluateHand(cards: string[]): handRanking {
-        cards.forEach(card => {
-            const cardValue = card.slice(0, -1);
-            const currentCount = this.counts.get(cardValue) || 0;
-            this.counts.set(cardValue, currentCount + 1);
-        })
-        
-        for (const count of this.counts.values()) {
-            if (this.isPair(count)) {
-                if (this.handRank.hasOnePair) {
-                    this.handRank.hasTwoPair = true;
-                    this.handRank.hasOnePair = false;
-                }else if (! this.handRank.hasTwoPair ){
-                    this.handRank.hasOnePair = true;
-                }
-            }
-            
+  evaluateHand(cards: string[]): handRanking {
+    cards.forEach((card) => {
+      const cardValue = card.slice(0, -1);
+      const currentCount = this.faceValueCounts.get(cardValue) || 0;
+      this.faceValueCounts.set(cardValue, currentCount + 1);
+    });
+    cards.forEach((card) => {
+        const suitValue = card.slice(1, -1);
+        const currentCount = this.suitValueCounts.get(suitValue) || 0;
+        this.suitValueCounts.set(suitValue, currentCount + 1);
+      });
+
+    for (const count of this.faceValueCounts.values()) {
+      if (this.isPair(count)) {
+        if (this.handRank.hasOnePair) {
+          this.handRank.hasTwoPair = true;
+          this.handRank.hasOnePair = false;
+        } else if (!this.handRank.hasTwoPair) {
+          this.handRank.hasOnePair = true;
         }
-        
-        return this.handRank
+      }
     }
 
-    private isPair(count: number): boolean  { return count === 2 }
+    return this.handRank;
+  }
+
+  private isPair(count: number): boolean {
+    return count === 2;
+  }
 }
