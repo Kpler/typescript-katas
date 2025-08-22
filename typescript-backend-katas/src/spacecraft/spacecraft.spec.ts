b5e0b23f2128bde6b1eadb94e9326fb17d3c9ee4
Nicolas Guignard <nicolas065@gmail.com>
2025-08-22 15:00:44 +0200

    wip

--- typescript-backend-katas/src/spacecraft/spacecraft.spec.ts
+++ typescript-backend-katas/src/spacecraft/spacecraft.spec.ts
@@ -1,14 +1,42 @@
 import { navigate2DSpacecraft } from './spacecraft';
 
 describe('Spacecraft', () => {
-  it('should execute additions and soustractions correctly', () => {
+  it('should stay at the initial position when no commands are sent', () => {
     expect(
-      navigate2DSpacecraft({
-        initial: {
-              position: [0, 0],
-            direction:
+      navigate2DSpacecraft(
+        {
+          position: [0, 0],
+          direction: 'N',
+          fuel: 100,
+          status: 'OK'
         },
-      })
-    ).toBe(3);
+        [],
+        [],
+      )
+    ).toStrictEqual({
+      position: [0, 0],
+      direction: 'N',
+      fuel: 100,
+      status: 'OK'
+    });
+  });
+  it('should move forward in the current direction', () => {
+    expect(
+      navigate2DSpacecraft(
+        {
+          position: [0, 0],
+          direction: 'N',
+          fuel: 100,
+          status: 'OK'
+        },
+        ['F'],
+        [],
+      )
+    ).toStrictEqual({
+      position: [0, 1],
+      direction: 'N',
+      fuel: 100,
+      status: 'OK'
+    });
   });
 });
