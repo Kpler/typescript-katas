3de8dd46c52f3690a3c2aa9b295a32f70614b0ae
Pedro Mendes <pmendes@kpler.com>
2025-10-29 14:19:01 +0000

    pedro turn 1

--- typescript-backend-katas/src/autumn-cleaning/autumn.spec.ts
+++ typescript-backend-katas/src/autumn-cleaning/autumn.spec.ts
@@ -3,7 +3,11 @@ import { assignTasks } from './autumn';
 describe('Autumn Cleaning - Level 0: Basic Matching', () => {
     describe('assignTasks', () => {
         it('should assign a task to a team based on simple type matching', () => {
-            
+            const volunteers = [{ name: "Lara", stamina: 80, available: true, skillLevel: 3, id: "1" }];
+            const tasks = [{ id: "t1", type: "leaves" as const, difficulty: 2 }];
+            const assignmentRules = [{ match: { type: "leaves" as const}, team: "Leaf Team" }];
+
+            const result = assignTasks(volunteers, tasks, assignmentRules);
         });
     });
 });
