501c7d6e0f9360c3c1ae7bbc5f62a47e16840772
vgarcin <vperiegarcin@kpler.com>
2025-10-29 15:24:07 +0100

    add first test

--- typescript-backend-katas/src/autumn-cleaning/autumn.spec.ts
+++ typescript-backend-katas/src/autumn-cleaning/autumn.spec.ts
@@ -3,7 +3,16 @@ import { assignTasks } from './autumn';
 describe('Autumn Cleaning - Level 0: Basic Matching', () => {
     describe('assignTasks', () => {
         it('should assign a task to a team based on simple type matching', () => {
-            
+            const assignment = assignTasks(
+              [{ name: "Lara", stamina: 80, available: true, skillLevel: 3, id: "1" }],
+              [{ id: "t1", type: "leaves", difficulty: 2 }],
+              [{ match: { type: "leaves" }, team: "Leaf Team" }]
+            );
+
+            expect(assignment ["Leaf Team"])
+            expect(assignment["Leaf Team"],[ { volunteer: {name: "Lara", stamina: 80, available: true, skillLevel: 3, id: "1" },
+                    task: { id: "t1", type: "leaves", difficulty: 2 } } ] )
+
         });
     });
 });
