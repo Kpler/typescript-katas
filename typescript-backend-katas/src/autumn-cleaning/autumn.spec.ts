87575e0a600b95342a044bd71bb5317393e23cc8
Nabil Krause <nkrause@kpler.com>
2025-10-29 15:32:55 +0100

    feat: add second test

--- typescript-backend-katas/src/autumn-cleaning/autumn.spec.ts
+++ typescript-backend-katas/src/autumn-cleaning/autumn.spec.ts
@@ -9,10 +9,21 @@ describe('Autumn Cleaning - Level 0: Basic Matching', () => {
               [{ match: { type: "leaves" }, team: "Leaf Team" }]
             );
 
-            expect(assignment ["Leaf Team"])
-            expect(assignment["Leaf Team"],[ { volunteer: {name: "Lara", stamina: 80, available: true, skillLevel: 3, id: "1" },
-                    task: { id: "t1", type: "leaves", difficulty: 2 } } ] )
+            expect(Object.keys(assignment)).toContain('Leaf Team');
+            expect(assignment["Leaf Team"]).toEqual(
+                [ { volunteer: {name: "Lara", stamina: 80, available: true, skillLevel: 3, id: "1" },
+                task: { id: "t1", type: "leaves", difficulty: 2 } } ]
+            );
+        });
 
+        it('should return no volunteer for non matching task type', () => {
+            const assignment = assignTasks(
+              [{ name: "Lara", stamina: 80, available: true, skillLevel: 3, id: "1" }],
+              [{ id: "t1", type: "compost", difficulty: 2 }],
+              [{ match: { type: "leaves" }, team: "Leaf Team" }]
+            );
+
+            expect(assignment).toEqual({'Leaf Team': []});
         });
     });
 });
--- typescript-backend-katas/src/autumn-cleaning/autumn.ts
+++ typescript-backend-katas/src/autumn-cleaning/autumn.ts
@@ -28,6 +28,13 @@ export function assignTasks(
     tasks: Task[],
     rules: AssignmentRule[],
     defaultTeam?: string
-): { [team: string]: { volunteer: Volunteer; task: Task }[] }
-
-// → { "Leaf Team": [ { volunteer: ..., task: ... } ] }
+): { [team: string]: { volunteer: Volunteer; task: Task }[] } {
+    return tasks.some(task => task.type === 'leaves') ? {
+        'Leaf Team':
+            [
+                { volunteer: {name: "Lara", stamina: 80, available: true, skillLevel: 3, id: "1" },
+                  task: { id: "t1", type: "leaves", difficulty: 2 }
+                }
+            ]
+    } : {'Leaf Team': []}
+}
