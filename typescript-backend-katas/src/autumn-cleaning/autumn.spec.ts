42edb3a726d72cb6e5c4986f0662bf8a264a1c3c
vgarcin <vperiegarcin@kpler.com>
2025-10-29 15:53:21 +0100

    wip

--- typescript-backend-katas/src/autumn-cleaning/autumn.spec.ts
+++ typescript-backend-katas/src/autumn-cleaning/autumn.spec.ts
@@ -39,6 +39,40 @@ describe('Autumn Cleaning - Level 0: Basic Matching', () => {
                     task: { id: "t1", type: "branches", difficulty: 2 } } ]
             );
         });
+
+        it('should return a volunteer for a matching branches task type', () => {
+            const assignment = assignTasks(
+              [
+                  { name: "Bob", stamina: 80, available: true, skillLevel: 5, id: "2" },
+                  { name: "Lara", stamina: 80, available: true, skillLevel: 3, id: "1" },
+              ],
+              [{ id: "t1", type: "branches", difficulty: 2 }],
+              [{ match: { type: "branches" }, team: "Leaf Team" }]
+            );
+
+            expect(Object.keys(assignment)).toContain('Leaf Team');
+            expect(assignment["Leaf Team"]).toEqual(
+              [ { volunteer: { name: "Bob", stamina: 80, available: true, skillLevel: 5, id: "2" },
+                  task: { id: "t1", type: "branches", difficulty: 2 } } ]
+            );
+        });
+
+        it('should return a volunteer with skill level', () => {
+            const assignment = assignTasks(
+              [
+                  { name: "Lara", stamina: 80, available: true, skillLevel: 1, id: "1" },
+                  { name: "Bob", stamina: 80, available: true, skillLevel: 5, id: "2" },
+              ],
+              [{ id: "t1", type: "branches", difficulty: 2 }],
+              [{ match: { type: "branches" }, team: "Leaf Team" }]
+            );
+
+            expect(Object.keys(assignment)).toContain('Leaf Team');
+            expect(assignment["Leaf Team"]).toEqual(
+              [ { volunteer: { name: "Bob", stamina: 80, available: true, skillLevel: 5, id: "2" },
+                  task: { id: "t1", type: "branches", difficulty: 2 } } ]
+            );
+        });
     });
 });
 
