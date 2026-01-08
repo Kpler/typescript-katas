c8e50b1d24e3697525affff0fda12c68ef6ba15b
Jan Hasselberg <hasselberg.jan@icloud.com>
2026-01-08 13:17:22 +0100

    feat(workshop): add Gift class properties and initial test case

--- typescript-backend-katas/src/christmas/workshop.spec.ts
+++ typescript-backend-katas/src/christmas/workshop.spec.ts
@@ -1,5 +1,15 @@
 import {Gift} from "./workshop";
 
 describe('Level 1: Gift Registry', () => {
-
+    test('a gift should have id, weight, recipient and name', () => {
+        const gift = new Gift(
+            8,
+            "Jan",
+            "New Laptop"
+        )
+        expect(gift.getId()).not.toBe(null)
+        expect(gift.getWeight()).toBe(8);
+        expect(gift.getRecipient()).toEqual("Jan");
+        expect(gift.getName()).toEqual("New Laptop");
+    })
 });
